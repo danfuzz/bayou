8b9ce074e6634c3dde294e7278fc291ed1fcf377
Dan Bornstein <danfuzz@milk.com>
2016-10-03 08:53:37 -0700

    Remove outdated comment.

--- client/js/app.js
+++ client/js/app.js
@@ -9,11 +9,6 @@
  */
 
 import Quill from 'quill';
-
-// Because we aren't building a unified bundle, this `Parchment` isn't actually
-// the same class that the Quill internals refer to by that name. This will
-// probably lead to weirdness, which is why -- TODO -- we need to get a unified
-// bundle to be built.
 import Parchment from 'parchment';
 
 // Demonstrates that Webpack conversion and bundling is working as expected.
