a0cf762958910bcf1ed08e72edd6d41f820a6f3a
Dan Bornstein <danfuzz@milk.com>
2016-10-17 15:57:03 -0700

    Update the `ts-loader` dependency.
    This obviates the need for the post-install patch.

--- scripts/fix-server-modules.sh
+++ scripts/fix-server-modules.sh
@@ -9,13 +9,18 @@
 # Expected to point at the base `server` directory.
 dir="$1"
 
+# As of this writing, there are no patches that need to be made. After the
+# `exit` is an example of a patch that has since been fixed, which might help
+# serve as a template for future patches.
+exit
+
 # This is already fixed in `master` in the `ts-loader` module, but as of this
 # writing it has not yet been published to npm. See
 # <https://github.com/TypeStrong/ts-loader/pull/202> for details.
 badText='path\.dirname\(configFilePath\)'
 goodText='path.dirname(configFilePath || "")'
 file="${dir}/node_modules/ts-loader/index.js"
-if grep --quiet -E "${badText}" "${file}"; then
+if [[ ! -e "${file}~" ]]; then
     sed -i '~' -E "s/${badText}/${goodText}/g" "${file}"
     if [[ "$(diff --brief "${file}" "${file}~")" == '' ]]; then
         echo 'Upstream seems to have been fixed! Huzzah!' 1>&2
