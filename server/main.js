c025bac3d26b4ec7d3863d6cf0aa5a6f47efdaeb
Dan Bornstein <danfuzz@milk.com>
2016-11-11 13:36:12 -0800

    Better logging for websocket connections.

--- server/main.js
+++ server/main.js
@@ -42,32 +42,77 @@ if (process.argv[2] === '--dev') {
 /** The single document managed by this instance. */
 const theDoc = new Document();
 
+/** The single webserver run by this instance. */
 const app = express();
-express_ws(app);
 
-// An abbreviated and colorized log to the console.
-app.use(morgan('dev', {stream: log.infoStream}));
-
-// A fairly complete log written to a file.
-const logStream =
-  fs.createWriteStream(path.resolve(baseDir, 'access.log'), {flags: 'a'});
-app.use(morgan('common', {stream: logStream}));
-
-// Map Quill files into `/static/quill`. This is used for CSS files but not for
-// the JS code; the JS code is included in the overall JS bundle file.
-app.use('/static/quill',
-  express.static(path.resolve(baseDir, 'client/node_modules/quill/dist')));
-
-// Use Webpack to serve a JS bundle.
-app.get('/static/bundle.js', new ClientBundle().requestHandler);
-
-// Find HTML files and other static assets in `client/assets`. This includes the
-// top-level `index.html` and `favicon`, as well as stuff under `static/`.
-app.use('/', express.static(path.resolve(baseDir, 'client/assets')));
-
-// Attach the API server.
-app.ws('/api', (ws, req) => { new ApiServer(ws, theDoc); });
+addRequestLogging();
+addRoutes();
 
 app.listen(PORT, function () {
   log.info(`Quillex listening on port ${PORT}.`);
 });
+
+/**
+ * Sets up logging for webserver requests.
+ */
+function addRequestLogging() {
+  // Stream to write to, when logging to a file.
+  const logStream =
+    fs.createWriteStream(path.resolve(baseDir, 'access.log'), {flags: 'a'});
+
+  // These log regular (non-websocket) requests at the time of completion,
+  // including a short colorized form to the console and a longer form to a
+  // file.
+
+  app.use(morgan('dev', {
+    stream: log.infoStream
+  }));
+
+  app.use(morgan('common', {
+    stream: logStream
+  }));
+
+  // These log websocket requests, at the time of request start (not at the
+  // time of completion because these are long-lived requests).
+
+  // Log skip function: Returns `true` for anything other than a websocket
+  // request.
+  function skip(req, res) {
+    return (req.get('upgrade') !== 'websocket');
+  }
+
+  app.use(morgan('WS :url', {
+    stream:    log.infoStream,
+    immediate: true,
+    skip:      skip
+  }));
+
+  app.use(morgan('common', {
+    stream:    logStream,
+    immediate: true,
+    skip:      skip
+  }));
+}
+
+/**
+ * Sets up the webserver routes.
+ */
+function addRoutes() {
+  // Make the webserver able to handle websockets.
+  express_ws(app);
+
+  // Map Quill files into `/static/quill`. This is used for CSS files but not for
+  // the JS code; the JS code is included in the overall JS bundle file.
+  app.use('/static/quill',
+    express.static(path.resolve(baseDir, 'client/node_modules/quill/dist')));
+
+  // Use Webpack to serve a JS bundle.
+  app.get('/static/bundle.js', new ClientBundle().requestHandler);
+
+  // Find HTML files and other static assets in `client/assets`. This includes the
+  // top-level `index.html` and `favicon`, as well as stuff under `static/`.
+  app.use('/', express.static(path.resolve(baseDir, 'client/assets')));
+
+  // Attach the API server.
+  app.ws('/api', (ws, req) => { new ApiServer(ws, theDoc); });
+}
