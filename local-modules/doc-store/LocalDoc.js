158756cbdffc2e7138e4241c72530f7d1a356224
Dan Bornstein <danfuzz@slack-corp.com>
2017-02-15 10:24:51 -0800

    Remove unused `import`.

--- local-modules/doc-store/LocalDoc.js
+++ local-modules/doc-store/LocalDoc.js
@@ -2,8 +2,6 @@
 // Licensed AS IS and WITHOUT WARRANTY under the Apache License,
 // Version 2.0. Details: <http://www.apache.org/licenses/LICENSE-2.0>
 
-import Typecheck from 'typecheck';
-
 import BaseDoc from './BaseDoc';
 
 /**
