8a4fc87eabe09562345c97d000699ce8e928ae8b
Dan Bornstein <danfuzz@slack-corp.com>
2017-02-14 16:41:43 -0800

    New module `doc-store`.
    This is the (start of the) infrastructure through which real storage back-ends
    can be integrated.

---
+++ local-modules/doc-store/main.js
@@ -0,0 +1,10 @@
+// Copyright 2016-2017 the Bayou Authors (Dan Bornstein et alia).
+// Licensed AS IS and WITHOUT WARRANTY under the Apache License,
+// Version 2.0. Details: <http://www.apache.org/licenses/LICENSE-2.0>
+
+import BaseDoc from './BaseDoc';
+import BaseDocStore from './BaseDocStore';
+import LocalDoc from './LocalDoc';
+import LocalDocStore from './LocalDocStore';
+
+export { BaseDoc, BaseDocStore, LocalDoc, LocalDocStore };
