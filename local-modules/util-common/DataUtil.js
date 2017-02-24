6de1a36dd8e291c72dc872688a281e8435f85010
Dan Bornstein <danfuzz@slack-corp.com>
2017-02-24 08:56:49 -0800

    Rework `deepFreeze()` to not be so JSON-focused.

--- local-modules/util-common/DataUtil.js
+++ local-modules/util-common/DataUtil.js
@@ -3,12 +3,13 @@
 // Version 2.0. Details: <http://www.apache.org/licenses/LICENSE-2.0>
 
 /**
- * "Data value" helper utilities. A "data value" is defined as a JavaScript
- * value or object which can be encoded as JSON without loss of fidelity. For
- * example, this means that arrays don't have "holes" or extra properties,
- * that object values are all just other data values (e.g. _not_ methods or
- * synthetic properties), objects only have `Object.prototype` as their
- * prototype, etc.
+ * "Data value" helper utilities. A "data value" is defined as any JavaScript
+ * value or object which has no behavior. This includes anything that can be
+ * encoded as JSON without loss of fidelity and also includes things such as
+ * `undefined`, symbols, and "odd-shaped" arrays (ones with holes or extra
+ * properties). It notably does _not_ include functions, objects with synthetic
+ * properties, or objects with a prototype other than `Object.prototype` or
+ * `Array.prototype`.
  */
 export default class DataUtil {
   /**
@@ -19,40 +20,55 @@ export default class DataUtil {
    * @returns {*} The deep-frozen version of `value`.
    */
   static deepFreeze(value) {
-    if (typeof value !== 'object') {
-      return value;
-    }
-
-    let newObj = value;
-    let any = false; // Becomes `true` the first time a change is made.
-
-    if (Array.isArray(value)) {
-      for (let i = 0; i < value.length; i++) {
-        const oldValue = value[i];
-        const newValue = DataUtil.deepFreeze(oldValue);
-        if (oldValue !== newValue) {
-          if (!any) {
-            newObj = value.slice(); // Clone the array.
-            any = true;
-          }
-          newObj[i] = newValue;
-        }
+    switch (typeof value) {
+      case 'boolean':
+      case 'number':
+      case 'string':
+      case 'symbol':
+      case 'undefined': {
+        // Pass through as-is.
+        return value;
       }
-    } else {
-      for (const k in value) {
-        const oldValue = value[k];
-        const newValue = DataUtil.deepFreeze(oldValue);
-        if (oldValue !== newValue) {
-          if (!any) {
-            newObj = Object.assign({}, value); // Clone the object.
-            any = true;
-          }
-          newObj[k] = newValue;
+
+      case 'object': {
+        if (value === null) {
+          // Pass through as-is.
+          return value;
         }
+
+        const proto = Object.getPrototypeOf(value);
+        let cloneBase = null; // Empty object to start from when cloning.
+
+        if (proto === Object.prototype) {
+          cloneBase = {};
+        } else if (proto === Array.prototype) {
+          cloneBase = [];
+        } else {
+          throw new Error(`Cannot deep-freeze non-data object: ${value}`);
+        }
+
+        let newObj = value;
+        let any = false; // Becomes `true` the first time a change is made.
+
+        for (const k in value) {
+          const oldValue = value[k];
+          const newValue = DataUtil.deepFreeze(oldValue);
+          if (oldValue !== newValue) {
+            if (!any) {
+              newObj = Object.assign(cloneBase, value); // Clone the object.
+              any = true;
+            }
+            newObj[k] = newValue;
+          }
+        }
+
+        Object.freeze(newObj);
+        return newObj;
+      }
+
+      default: {
+        throw new Error(`Cannot deep-freeze non-data value: ${value}`);
       }
     }
-
-    Object.freeze(newObj);
-    return newObj;
   }
 }
