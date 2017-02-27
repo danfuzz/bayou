8fe30f84d2b9d8cd1b09909535f1c6cd6e2a7cbb
Dan Bornstein <danfuzz@slack-corp.com>
2017-02-27 12:37:17 -0800

    Switch theme, and pare down formatting options.
    From @meantime (I'm just the commit-monkey).

--- local-modules/quill-util/QuillMaker.js
+++ local-modules/quill-util/QuillMaker.js
@@ -11,16 +11,10 @@ const DEFAULT_TOOLBAR_CONFIG = [
   ['bold', 'italic', 'underline', 'strike', 'code'], // toggled buttons
   ['blockquote', 'code-block'],
 
-  [{header: 1}, {header: 2}],                    // custom button values
   [{list: 'ordered'}, {list: 'bullet'}],
-  [{script: 'sub'}, {script: 'super'}],          // superscript/subscript
-  [{indent: '-1'}, {indent: '+1'}],              // outdent/indent
 
-  [{size: ['small', false, 'large', 'huge']}],   // custom dropdown
-  [{header: [1, 2, 3, 4, 5, 6, false]}],
+  [{header: [1, 2, 3, 4, false]}],
 
-  [{color: []}, {background: []}],               // dropdown with defaults from theme
-  [{font: []}],
   [{align: []}],
 
   ['clean']                                      // remove formatting button
@@ -54,7 +48,7 @@ export default class QuillMaker {
     const result = new QuillProm(id, {
       readOnly: true,
       strict: true,
-      theme: 'snow',
+      theme: 'bubble',
       modules: {
         toolbar: toolbarConfig
       }
