c92db9e6f67e709545547eca80d81eacf56c2c5a
Dan Bornstein <danfuzz@slack-corp.com>
2017-02-28 16:49:34 -0800

    Flesh out handling of POSTs.

--- local-modules/api-server/PostConnection.js
+++ local-modules/api-server/PostConnection.js
@@ -26,9 +26,51 @@ export default class PostConnection extends Connection {
     /** {object} The HTTP response. */
     this._res = res;
 
-    res
-      .status(200)
-      .type('application/json')
-      .send('{"id": -1, "error": "TODO"}\n');
+    /** {Array<Buffer>} The request POST payload, as individual chunks. */
+    this._chunks = [];
+
+    req.on('data', this._handleData.bind(this));
+    req.on('end', this._handleEnd.bind(this));
+    req.on('error', this._handleError.bind(this));
+  }
+
+  /**
+   * Handles a `data` event coming from the request input stream.
+   *
+   * @param {Buffer} chunk Incoming data chunk.
+   */
+  _handleData(chunk) {
+    this._log.detail('Chunk:', chunk);
+    this._chunks.push(chunk);
+  }
+
+  /**
+   * Handles an `end` event coming from the request input stream.
+   */
+  _handleEnd() {
+    this._log.info('Close.');
+
+    const msg = Buffer.concat(this._chunks).toString('utf8');
+    this.handleJsonMessage(msg).then(
+      (response) => {
+        this._res
+          .status(200)
+          .type('application/json')
+          .send(response);
+      });
+  }
+
+  /**
+   * Handles an `error` event coming from the underlying connection.
+   *
+   * @param {object} error The error event.
+   */
+  _handleError(error) {
+    this._log.info('Error:', error);
+
+    this._res
+    .status(400) // "Bad Request" status code.
+    .type('application/json')
+    .send('{"id": -1, "error": "Trouble receiving POST payload."}\n');
   }
 }
