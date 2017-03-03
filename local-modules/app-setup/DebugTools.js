6350628bb1932a86de2bab024f7a334d2dccb31d
Dan Bornstein <danfuzz@slack-corp.com>
2017-03-03 11:29:12 -0800

    Factor out parameter checking.

--- local-modules/app-setup/DebugTools.js
+++ local-modules/app-setup/DebugTools.js
@@ -3,9 +3,14 @@
 // Version 2.0. Details: <http://www.apache.org/licenses/LICENSE-2.0>
 
 import express from 'express';
+import util from 'util';
 
 import { Encoder } from 'api-common';
+import { SeeAll } from 'see-all';
 import { SeeAllRecent } from 'see-all-server';
+
+/** Logger for this module. */
+const log = new SeeAll('app-debug');
 
 /** How long a log to maintain, in msec. */
 const LOG_LENGTH_MSEC = 1000 * 60 * 60; // One hour.
@@ -106,14 +111,64 @@ export default class DebugTools {
   }
 
   /**
+   * Validates a version number as a request parameter.
+   *
+   * @param {object} req_unused Express request.
+   * @param {object} res_unused Express response.
+   * @param {Function} next Next handler to call.
+   * @param {string} value Request parameter value.
+   * @param {string} name_unused Request parameter name.
+   */
+  _check_verNum(req_unused, res_unused, next, value, name_unused) {
+    if (!value.match(/^[0-9]+$/)) {
+      const error = new Error();
+      error.debugMsg = 'Bad value for `verNum`.';
+      throw error;
+    }
+
+    next();
+  }
+
+  /**
    * The request handler function, suitable for use with Express. Usable as-is
    * (without `.bind()`).
    */
   get requestHandler() {
     const router = new express.Router();
-    router.get(/^\/change\/[0-9]+$/,      this._change.bind(this));
-    router.get('/log',                    this._log.bind(this));
-    router.get(/^\/snapshot(\/[0-9]*)?$/, this._snapshot.bind(this));
+
+    router.get('/change/:verNum',   this._change.bind(this));
+    router.get('/log',              this._log.bind(this));
+    router.get('/snapshot',         this._snapshot.bind(this));
+    router.get('/snapshot/:verNum', this._snapshot.bind(this));
+
+    router.param('verNum', this._check_verNum.bind(this));
+
+    // Error handler. Express "knows" this is an error handler _explicitly_
+    // because it is defined to take four arguments. (Yeah, kinda precarious.)
+    router.use((err, req_unused, res, next_unused) => {
+      let text = 'Error while handling debug URL:\n\n';
+
+      if (err.debugMsg) {
+        // We added our own message. Use that instead of just dumping a
+        // stack trace.
+        text += `    ${err.debugMsg}\n`;
+      } else {
+        // If there was no error message, then this isn't just (something like)
+        // a user input error, so report the whole stack trace back.
+        //
+        // **Note:** It is reasonably safe to spew a stack trace back over the
+        // connection because we should only be running code in this file at all
+        // if the product is running in a dev (not production) configuration.
+        log.error(err);
+        text += util.inspect(err);
+      }
+
+      res
+        .status(500)
+        .type('text/plain')
+        .send(text);
+    });
+
     return router;
   }
 }
