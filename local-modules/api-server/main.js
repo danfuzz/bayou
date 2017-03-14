2279930195211172a407e7cc9825745ec8f192f6
Dan Bornstein <danfuzz@slack-corp.com>
2017-03-14 11:51:08 -0700

    Export `BearerToken`.

--- local-modules/api-server/main.js
+++ local-modules/api-server/main.js
@@ -2,12 +2,11 @@
 // Licensed AS IS and WITHOUT WARRANTY under the Apache License,
 // Version 2.0. Details: <http://www.apache.org/licenses/LICENSE-2.0>
 
+import BearerToken from './BearerToken';
 import Connection from './Connection';
 import Context from './Context';
 import PostConnection from './PostConnection';
 import Target from './Target';
 import WsConnection from './WsConnection';
 
-// TODO: Export `BearerToken` once circular dependency issues have been
-// resolved.
-export { /*BearerToken,*/ Connection, Context, PostConnection, Target, WsConnection };
+export { BearerToken, Connection, Context, PostConnection, Target, WsConnection };
