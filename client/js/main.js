07b95005e48c3ff9214fdb77caef3b8322f59a7b
Dan Bornstein <danfuzz@slack-corp.com>
2017-03-20 16:26:17 -0700

    Wait for page load before hooking things up.

--- client/js/main.js
+++ client/js/main.js
@@ -14,6 +14,7 @@ import { Decoder } from 'api-common';
 import { DocClient } from 'doc-client';
 import { Hooks } from 'hooks-client';
 import { QuillMaker } from 'quill-util';
+import { SeeAll } from 'see-all';
 import { SeeAllBrowser } from 'see-all-browser';
 
 // Pull the incoming parameters from `window.` globals into locals, to prevent
@@ -29,6 +30,7 @@ if (!(BAYOU_KEY && BAYOU_NODE)) {
 
 // Init logging.
 SeeAllBrowser.init();
+const log = new SeeAll('page-init');
 
 // Figure out the URL of our server. We use the `BAYOU_KEY` global specified by
 // the enclosing HTML. We don't just _always_ use the document's URL because it
@@ -50,26 +52,44 @@ if (baseUrl.length === 0) {
   throw new Error(`Could not determine base URL of: ${url}`);
 }
 
-// Give the overlay a chance to do any initialization.
-Hooks.run(window, baseUrl);
+// Arrange for the rest of initialization to happen once the initial page
+// contents are fully loaded.
+window.addEventListener('load', (event_unused) => {
+  log.info('Initial page load complete.');
 
-// Figure out what node we're attaching the editor to. We use the `BAYOU_NODE`
-// global specified by the enclosing HTML.
-if (document.querySelector(BAYOU_NODE) === null) {
-  const extra = (BAYOU_NODE[0] === '#') ? '' : ' (maybe need a `#` prefix?)';
-  throw new Error(`No such selector${extra}: \`${BAYOU_NODE}\``);
-}
+  // Figure out what node we're attaching the editor to. We use the `BAYOU_NODE`
+  // global specified by the enclosing HTML.
+  if (document.querySelector(BAYOU_NODE) === null) {
+    // If we land here, no further init can possibly be done, so we just
+    // `return` out of it.
+    const extra = (BAYOU_NODE[0] === '#') ? '' : ' (maybe need a `#` prefix?)';
+    log.error(`No such selector${extra}: \`${BAYOU_NODE}\``);
+    return;
+  }
 
-// Make the editor instance.
-const quill = QuillMaker.make(BAYOU_NODE);
+  // Give the overlay a chance to do any initialization.
+  log.detail('Running `run()` hook...');
+  Hooks.run(window, baseUrl);
 
-// Initialize the API connection, and hook it up to the Quill instance. Similar
-// to the node identification (immediately above), we use the URL inside the
-// `BAYOU_KEY`, if that's passed, falling back on the document URL for the
-// soon-to-be legacy case. TODO: Should probably insist on `BAYOU_KEY` being
-// defined.
+  // Make the editor instance.
+  log.detail('Making editor instance...');
+  const quill = QuillMaker.make(BAYOU_NODE);
 
-const apiClient = new ApiClient(baseUrl);
-apiClient.open();
-const docClient = new DocClient(quill, apiClient);
-docClient.start();
+  // Initialize the API connection, and hook it up to the Quill instance. Similar
+  // to the node identification (immediately above), we use the URL inside the
+  // `BAYOU_KEY`, if that's passed, falling back on the document URL for the
+  // soon-to-be legacy case. TODO: Should probably insist on `BAYOU_KEY` being
+  // defined.
+
+  log.detail('Opening API client...');
+  const apiClient = new ApiClient(baseUrl);
+  apiClient.open().then(() => {
+    log.detail('API client open.');
+  });
+
+  log.detail('Hooking up document client...');
+  const docClient = new DocClient(quill, apiClient);
+  docClient.start();
+
+  log.detail('Done(ish). Async operations now in progress...');
+});
