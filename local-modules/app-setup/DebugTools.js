7b649e7945d738debba3e357638b0b1b0530baa5
Dan Bornstein <danfuzz@slack-corp.com>
2017-06-14 12:27:23 -0700

    Rework these handlers as `async`.

--- local-modules/app-setup/DebugTools.js
+++ local-modules/app-setup/DebugTools.js
@@ -208,37 +208,34 @@ export default class DebugTools {
    *
    * @param {object} req HTTP request.
    * @param {object} res HTTP response handler.
-   * @returns {Promise} Promise whose rejection indicates an error to be
-   *   reported back to the user.
    */
-  _handle_edit(req, res) {
+  async _handle_edit(req, res) {
     const documentId = req.params.documentId;
     const authorId   = this._getAuthorIdParam(req);
+    const key        = await this._makeEncodedKey(documentId, authorId);
 
-    return this._makeEncodedKey(documentId, authorId).then((key) => {
-      // These are already strings (JSON-encoded even, in the case of `key`),
-      // but we still have to JSON-encode _those_ strings, so as to make them
-      // proper JS source within the <script> block below.
-      const quotedKey        = JSON.stringify(key);
-      const quotedDocumentId = JSON.stringify(documentId);
-      const quotedAuthorId   = JSON.stringify(authorId);
+    // These are already strings (JSON-encoded even, in the case of `key`),
+    // but we still have to JSON-encode _those_ strings, so as to make them
+    // proper JS source within the <script> block below.
+    const quotedKey        = JSON.stringify(key);
+    const quotedDocumentId = JSON.stringify(documentId);
+    const quotedAuthorId   = JSON.stringify(authorId);
 
-      // TODO: Probably want to use a real template.
-      const head =
-        '<title>Editor</title>\n' +
-        '<script>\n' +
-        `  BAYOU_KEY         = ${quotedKey};\n` +
-        '  BAYOU_NODE        = "#editor";\n' +
-        `  DEBUG_AUTHOR_ID   = ${quotedAuthorId};\n` +
-        `  DEBUG_DOCUMENT_ID = ${quotedDocumentId};\n` +
-        '</script>\n' +
-        '<script src="/boot-for-debug.js"></script>\n';
-      const body =
-        '<h1>Editor</h1>\n' +
-        '<div id="editor"><p>Loading&hellip;</p></div>\n';
+    // TODO: Probably want to use a real template.
+    const head =
+      '<title>Editor</title>\n' +
+      '<script>\n' +
+      `  BAYOU_KEY         = ${quotedKey};\n` +
+      '  BAYOU_NODE        = "#editor";\n' +
+      `  DEBUG_AUTHOR_ID   = ${quotedAuthorId};\n` +
+      `  DEBUG_DOCUMENT_ID = ${quotedDocumentId};\n` +
+      '</script>\n' +
+      '<script src="/boot-for-debug.js"></script>\n';
+    const body =
+      '<h1>Editor</h1>\n' +
+      '<div id="editor"><p>Loading&hellip;</p></div>\n';
 
-      this._htmlResponse(res, head, body);
-    });
+    this._htmlResponse(res, head, body);
   }
 
   /**
@@ -247,16 +244,13 @@ export default class DebugTools {
    *
    * @param {object} req HTTP request.
    * @param {object} res HTTP response handler.
-   * @returns {Promise} Promise whose rejection indicates an error to be
-   *   reported back to the user.
    */
-  _handle_key(req, res) {
+  async _handle_key(req, res) {
     const documentId = req.params.documentId;
     const authorId   = this._getAuthorIdParam(req);
+    const key        = await this._makeEncodedKey(documentId, authorId);
 
-    return this._makeEncodedKey(documentId, authorId).then((key) => {
-      this._jsonResponse(res, key);
-    });
+    this._jsonResponse(res, key);
   }
 
   /**
