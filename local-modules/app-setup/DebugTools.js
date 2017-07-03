736b1906ebc94d65f26da9be69bc55b389b43619
Dan Bornstein <danfuzz@slack-corp.com>
2017-07-03 16:59:26 -0700

    Rename `bayou-content-wrapper` -> `bayou-top`.
    It's the new top.

--- local-modules/app-setup/DebugTools.js
+++ local-modules/app-setup/DebugTools.js
@@ -233,7 +233,7 @@ export default class DebugTools {
       '<script src="/boot-for-debug.js"></script>\n';
     const body =
       '<h1>Editor</h1>\n' +
-      '<div class="bayou-content-wrapper">\n' +
+      '<div class="bayou-top">\n' +
         '<svg class="bayou-author-overlay"></svg>' +
         '<div id="editor"><p>Loading&hellip;</p></div>\n' +
       '</div>';
