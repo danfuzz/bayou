aa07fd7987c071aa5b70e7ce698e21b9dcaaa135
Dan Bornstein <danfuzz@slack-corp.com>
2017-07-06 15:45:43 -0700

    Sort methods.
    Because I'm like that.

--- local-modules/doc-server/AuthorSession.js
+++ local-modules/doc-server/AuthorSession.js
@@ -36,6 +36,22 @@ export default class AuthorSession {
   }
 
   /**
+   * Applies a delta, assigning authorship of the change to the author
+   * represented by this instance. See the equivalent `DocControl` method for
+   * details.
+   *
+   * @param {number} baseRevNum Revision number which `delta` is with respect
+   *   to.
+   * @param {FrozenDelta} delta Delta indicating what has changed with respect
+   *   to `baseRevNum`.
+   * @returns {Promise<DeltaResult>} Promise for the correction from the
+   *   implied expected result to get the actual result.
+   */
+  applyDelta(baseRevNum, delta) {
+    return this._doc.applyDelta(baseRevNum, delta, this._authorId);
+  }
+
+  /**
    * Returns a particular change to the document. See the equivalent
    * `DocControl` method for details.
    *
@@ -47,11 +63,24 @@ export default class AuthorSession {
   }
 
   /**
+   * Returns a promise for a snapshot of any revision after the given
+   * `baseRevNum`. See the equivalent `DocControl` method for details.
+   *
+   * @param {Int} baseRevNum Revision number for the document.
+   * @returns {Promise<DeltaResult>} Promise for a delta and associated revision
+   *   number. The result's `delta` can be applied to revision `baseRevNum` to
+   *   produce revision `revNum` of the document.
+   */
+  deltaAfter(baseRevNum) {
+    return this._doc.deltaAfter(baseRevNum);
+  }
+
+  /**
    * Returns a bit of identifying info about this instance, for the purposes of
    * logging. Specifically, the client side will call this method and log the
    * results during session initiation.
    *
-   * @returns {string} A succinct identification string
+   * @returns {string} A succinct identification string.
    */
   getLogInfo() {
     return `session ${this._sessionId}; doc ${this._doc.id}; author ${this._authorId}`;
@@ -76,34 +105,5 @@ export default class AuthorSession {
    */
   snapshot(revNum = null) {
     return this._doc.snapshot(revNum);
-  }
-
-  /**
-   * Returns a promise for a snapshot of any revision after the given
-   * `baseRevNum`. See the equivalent `DocControl` method for details.
-   *
-   * @param {Int} baseRevNum Revision number for the document.
-   * @returns {Promise<DeltaResult>} Promise for a delta and associated revision
-   *   number. The result's `delta` can be applied to revision `baseRevNum` to
-   *   produce revision `revNum` of the document.
-   */
-  deltaAfter(baseRevNum) {
-    return this._doc.deltaAfter(baseRevNum);
-  }
-
-  /**
-   * Applies a delta, assigning authorship of the change to the author
-   * represented by this instance. See the equivalent `DocControl` method for
-   * details.
-   *
-   * @param {number} baseRevNum Revision number which `delta` is with respect
-   *   to.
-   * @param {FrozenDelta} delta Delta indicating what has changed with respect
-   *   to `baseRevNum`.
-   * @returns {Promise<DeltaResult>} Promise for the correction from the
-   *   implied expected result to get the actual result.
-   */
-  applyDelta(baseRevNum, delta) {
-    return this._doc.applyDelta(baseRevNum, delta, this._authorId);
   }
 }
