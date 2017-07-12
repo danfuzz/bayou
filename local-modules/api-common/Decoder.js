5c60a281b8ded057d34a07f53e82c7fb24bade13
Dan Bornstein <danfuzz@slack-corp.com>
2017-07-12 15:50:37 -0700

    Collate the instance decode code into `_decodeInstance()`.

--- local-modules/api-common/Decoder.js
+++ local-modules/api-common/Decoder.js
@@ -38,27 +38,9 @@ export default class Decoder extends CommonBase {
       return this._decodeSimpleObject(value);
     } else if (!Array.isArray(value)) {
       throw new Error(`API cannot decode object of class \`${value.constructor.name}\`.`);
+    } else {
+      return this._decodeInstance(value);
     }
-
-    // We know it's encoded as an array, which means it's going to decode either
-    // to an array per se or to an arbitrary value via a registered item codec.
-    // Note that array results are handled by virtue of the fact that
-    // `SpecialCodecs.ARRAY` will have been registered as an item codec.
-
-    const tag = value[0];
-    const payload = value.slice(1);
-
-    // It's an error if the array doesn't start with a string tag (even for
-    // a value that decodes to an array per se), so check for that.
-    if (typeof tag !== 'string') {
-      if (value.length === 0) {
-        throw new Error('API cannot decode empty arrays.');
-      } else {
-        throw new Error('API cannot decode arrays without an initial string tag.');
-      }
-    }
-
-    return this._decodeInstance(tag, payload);
   }
 
   /**
@@ -79,13 +61,29 @@ export default class Decoder extends CommonBase {
 
   /**
    * Helper for `decodeData()` which validates and converts a tagged
-   * constructor array.
+   * constructor array. These are used as the encoded form of both arrays per se
+   * and arbitrary class instances.
    *
-   * @param {string} tag Name tag.
-   * @param {array} payload Construction arguments.
-   * @returns {object} The converted value.
+   * **Note:** Array results are handled by virtue of the fact that
+   * `SpecialCodecs.ARRAY` will have been registered as an item codec.
+   *
+   * @param {array<*>} encoded The encoded value.
+   * @returns {object} The decoded value.
    */
-  _decodeInstance(tag, payload) {
+  _decodeInstance(encoded) {
+    const tag       = encoded[0];
+    const payload   = encoded.slice(1);
+
+    // It's an error if the array doesn't start with a string tag (even for
+    // a value that decodes to an array per se), so check for that.
+    if (typeof tag !== 'string') {
+      if (encoded.length === 0) {
+        throw new Error('API cannot decode empty arrays.');
+      } else {
+        throw new Error('API cannot decode arrays without an initial string tag.');
+      }
+    }
+
     const itemCodec = this._reg.codecForTag(tag);
     const decodedPayload =
       Object.freeze(payload.map(this.decodeData.bind(this)));
