34baa47b994c7ea82e19ef2e0111fb50a7528a2b
Dan Bornstein <danfuzz@slack-corp.com>
2017-08-10 11:55:44 -0700

    Add tests for `StoragePath`.

---
+++ local-modules/content-store/tests/test_StoragePath.js
@@ -0,0 +1,191 @@
+// Copyright 2016-2017 the Bayou Authors (Dan Bornstein et alia).
+// Licensed AS IS and WITHOUT WARRANTY under the Apache License,
+// Version 2.0. Details: <http://www.apache.org/licenses/LICENSE-2.0>
+
+import { assert } from 'chai';
+import { describe, it } from 'mocha';
+
+import { StoragePath } from 'content-store';
+
+
+describe('content-store/StoragePath', () => {
+  describe('check()', () => {
+    it('should accept valid paths', () => {
+      function test(value) {
+        assert.strictEqual(StoragePath.check(value), value);
+      }
+
+      test('/a');
+      test('/1');
+      test('/_');
+      test('/abc123_ABC');
+      test('/foo/bar');
+      test('/foo/bar/999999');
+      test('/florp/blort/like/TIMELINE_GOES_SIDEWAYS');
+    });
+
+    it('should reject invalid paths', () => {
+      function test(value) {
+        assert.throws(() => { StoragePath.check(value); });
+      }
+
+      // No components.
+      test('');
+      test('/');
+      test('//');
+
+      // Improper slash hygiene.
+      test('a');
+      test('a/');
+      test('a/b');
+      test('a//b');
+      test('/a/');
+      test('//a/');
+      test('/a//');
+      test('/a//b');
+
+      // Invalid characters in components.
+      test('/!');
+      test('/~');
+      test('/@');
+      test('/#');
+      test('/foo!');
+      test('/bar~');
+      test('/baz@');
+      test('/blort#');
+      test('/foo/!a');
+      test('/bar/~b');
+      test('/baz/@c');
+      test('/blort/#d');
+    });
+
+    it('should reject non-strings', () => {
+      function test(value) {
+        assert.throws(() => { StoragePath.check(value); });
+      }
+
+      test(null);
+      test(undefined);
+      test(true);
+      test(123.456);
+      test([]);
+      test(['/hello']);
+      test({});
+      test({ '/x': '/y' });
+    });
+  });
+
+  describe('checkComponent()', () => {
+    it('should accept valid components', () => {
+      function test(value) {
+        assert.strictEqual(StoragePath.checkComponent(value), value);
+      }
+
+      test('a');
+      test('1');
+      test('_');
+      test('abc123_ABC');
+      test('foobar');
+      test('999999');
+      test('TIMELINE_GOES_SIDEWAYS');
+    });
+
+    it('should reject invalid components', () => {
+      function test(value) {
+        assert.throws(() => { StoragePath.checkComponent(value); });
+      }
+
+      test('');
+      test('/');
+      test('~');
+      test('@');
+      test('#');
+      test('/foo');
+      test('foo!');
+      test('bar~');
+      test('baz@');
+      test('blort#');
+      test('/foo/a');
+      test('bar~b');
+      test('baz@c');
+      test('blort#d');
+    });
+
+    it('should reject non-strings', () => {
+      function test(value) {
+        assert.throws(() => { StoragePath.checkComponent(value); });
+      }
+
+      test(null);
+      test(undefined);
+      test(true);
+      test(123.456);
+      test([]);
+      test(['hello']);
+      test({});
+      test({ x: 'y' });
+    });
+  });
+
+  describe('join()', () => {
+    it('should join as expected', () => {
+      function test(value, expected) {
+        assert.strictEqual(StoragePath.join(value), expected);
+      }
+
+      test(['a'], '/a');
+      test(['a', 'b'], '/a/b');
+      test(['blort', 'florp', 'like'], '/blort/florp/like');
+    });
+  });
+
+  describe('orNull()', () => {
+    it('should accept valid paths and `null`', () => {
+      function test(value) {
+        assert.doesNotThrow(() => { StoragePath.orNull(value); });
+      }
+
+      test('/a');
+      test('/foo/bar');
+      test(null);
+    });
+
+    it('should reject invalid paths', () => {
+      function test(value) {
+        assert.throws(() => { StoragePath.orNull(value); });
+      }
+
+      test('');
+      test('/');
+      test('a/');
+      test('a//b');
+      test('/!/x');
+    });
+
+    it('should reject non-strings', () => {
+      function test(value) {
+        assert.throws(() => { StoragePath.check(value); });
+      }
+
+      test(undefined);
+      test(false);
+      test(123.456);
+      test([]);
+      test(['/hello']);
+      test({});
+      test({ '/x': '/y' });
+    });
+  });
+
+  describe('split()', () => {
+    it('should split as expected', () => {
+      function test(value, expected) {
+        assert.deepEqual(StoragePath.split(value), expected);
+      }
+
+      test('/a', ['a']);
+      test('/a/b', ['a', 'b']);
+      test('/blort/florp/like', ['blort', 'florp', 'like']);
+    });
+  });
+});
