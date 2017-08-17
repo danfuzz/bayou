bd9cd4faf34db7f760f7f66909c4a56936a9a6d7
Dan Bornstein <danfuzz@slack-corp.com>
2017-08-17 09:23:58 -0700

    Extract all the compiler-related deps into their own module.
    This will help keep our "compiler regime" in synch across client and server.

---
+++ local-modules/compiler-dependencies/main.js
@@ -0,0 +1,12 @@
+// Copyright 2016-2017 the Bayou Authors (Dan Bornstein et alia).
+// Licensed AS IS and WITHOUT WARRANTY under the Apache License,
+// Version 2.0. Details: <http://www.apache.org/licenses/LICENSE-2.0>
+
+// **Note:** This file gets loaded by the `compiler` subproject, which means it
+// can only use JavaScript as supported directly by Node. This is why we use
+// `module.exports = ...` and not `export ...`. Not that we are exporting
+// anything anyway... just sayin'.
+
+// This module serves only to hold common dependencies, so there's nothing to
+// export.
+module.exports = {};
