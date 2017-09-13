3dabbb17234c403f0e47c38d36412bfc53300186
Dan Bornstein <danfuzz@slack-corp.com>
2017-09-13 13:39:04 -0700

    Remove unneeded import.

--- local-modules/see-all-client/ClientSink.js
+++ local-modules/see-all-client/ClientSink.js
@@ -3,7 +3,7 @@
 // Version 2.0. Details: <http://www.apache.org/licenses/LICENSE-2.0>
 
 import { SeeAll } from 'see-all';
-import { InfoError, Singleton } from 'util-common';
+import { Singleton } from 'util-common';
 
 /**
  * Implementation of the `see-all` logging sink protocol for use in a web
