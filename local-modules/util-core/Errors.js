f2e59fa68a8865dd56c9004a015d4078e470c676
Dan Bornstein <danfuzz@slack-corp.com>
2017-09-14 10:12:38 -0700

    A bit of philosophy.

--- local-modules/util-core/Errors.js
+++ local-modules/util-core/Errors.js
@@ -21,6 +21,10 @@ export default class Errors extends UtilityClass {
    * as an argument to a function (or similar). The error includes a description
    * (typically pseudocode-ish) of the expected type of value.
    *
+   * This error is typically used to report an error that crosses a line of
+   * responsibility, e.g. to report that a user of a class or module is breaking
+   * the contract by passing an out-of-spec value.
+   *
    * @param {*} value The bad value.
    * @param {string|function} expectedType Name of the expected type or a
    *   function (presumably a constructor/class) whose name is the expected
@@ -54,6 +58,15 @@ export default class Errors extends UtilityClass {
    * Constructs an instance which is meant to indicate that the program
    * exhibited unexpected behavior. This should be used as an indication of a
    * likely bug in the program.
+   *
+   * This error is typically used to report an error that _does not_ cross a
+   * line of responsibility. That is, it is intended to convey that the code at
+   * the point of failure "believed" it was being used properly and yet still
+   * ran into unexpected trouble. In other systems, you might see something like
+   * `throw new Error("Shouldn't happen.")` in this sort of situation.
+   *
+   * To be clear, this is _not_ an appropriate error to use to report "possible
+   * but still unusual and noteworthy" problems such as network failure.
    *
    * @param {*} message Human-oriented message with some indication of what
    *   went wrong.
