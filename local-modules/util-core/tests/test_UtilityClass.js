11a097291341131f209cb5c9bea43629e25c346d
Dan Bornstein <danfuzz@slack-corp.com>
2017-09-14 11:06:56 -0700

    Use `Errors.bad_use()`.

---
+++ local-modules/util-core/tests/test_UtilityClass.js
@@ -0,0 +1,26 @@
+// Copyright 2016-2017 the Bayou Authors (Dan Bornstein et alia).
+// Licensed AS IS and WITHOUT WARRANTY under the Apache License,
+// Version 2.0. Details: <http://www.apache.org/licenses/LICENSE-2.0>
+
+import { assert } from 'chai';
+import { describe, it } from 'mocha';
+
+import { UtilityClass } from 'util-core';
+
+describe('util-core/UtilityClass', () => {
+  describe('constructor()', () => {
+    it('should always throw an error', () => {
+      assert.throws(() => { new UtilityClass(); });
+    });
+
+    it('should always throw an error when called via `super`', () => {
+      class Subclass extends UtilityClass {
+        constructor() {
+          super();
+        }
+      }
+
+      assert.throws(() => { new Subclass(); });
+    });
+  });
+});
