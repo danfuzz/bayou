b912aeaa5dce1e5bff8dddebe4e9f84f8156e1ac
Dan Bornstein <danfuzz@slack-corp.com>
2017-09-14 11:13:16 -0700

    Use `Errors.bad_use()`.
    And expand tests.

--- local-modules/util-common/Singleton.js
+++ local-modules/util-common/Singleton.js
@@ -2,7 +2,7 @@
 // Licensed AS IS and WITHOUT WARRANTY under the Apache License,
 // Version 2.0. Details: <http://www.apache.org/licenses/LICENSE-2.0>
 
-import { CommonBase, ObjectUtil } from 'util-core';
+import { CommonBase, Errors, ObjectUtil } from 'util-core';
 
 /**
  * Base class for singletons (classes for which there is ever but a single
@@ -47,7 +47,7 @@ export default class Singleton extends CommonBase {
 
     // See the note in `theOne` above in re `hasOwnProperty`.
     if (ObjectUtil.hasOwnProperty(this.constructor, '_theOne')) {
-      throw new Error('Cannot re-instantiate singleton class.');
+      throw Errors.bad_use('Attempt to re-instantiate singleton class.');
     }
 
     this.constructor._theOne = this;
