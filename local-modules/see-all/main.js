14233ede83f2ba42083c2f116393700647cbd065
Dan Bornstein <danfuzz@slack-corp.com>
2017-09-19 15:02:39 -0700

    Add tests for `LogStream`.

--- local-modules/see-all/main.js
+++ local-modules/see-all/main.js
@@ -2,7 +2,9 @@
 // Licensed AS IS and WITHOUT WARRANTY under the Apache License,
 // Version 2.0. Details: <http://www.apache.org/licenses/LICENSE-2.0>
 
+import BaseLogger from './BaseLogger';
 import Logger from './Logger';
+import LogStream from './LogStream';
 import SeeAll from './SeeAll';
 
-export { Logger, SeeAll };
+export { BaseLogger, Logger, LogStream, SeeAll };
