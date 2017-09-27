4588cb641c928733e6d97cc4328a7060ad548d7b
Dan Bornstein <danfuzz@slack-corp.com>
2017-09-27 16:20:06 -0700

    Add tests for instance `isEmpty()`.

--- local-modules/doc-common/tests/test_BodyDelta.js
+++ local-modules/doc-common/tests/test_BodyDelta.js
@@ -103,7 +103,37 @@ describe('doc-common/BodyDelta', () => {
     });
   });
 
-  describe('isDocument(doc)', () => {
+  describe('isEmpty()', () => {
+    describe('valid empty values', () => {
+      const values = [
+        new BodyDelta([]),
+        BodyDelta.EMPTY,
+      ];
+
+      for (const v of values) {
+        it(`should return \`true\` for: ${inspect(v)}`, () => {
+          assert.isTrue(v.isEmpty());
+        });
+      }
+    });
+
+    describe('valid non-empty values', () => {
+      const values = [
+        [{ insert: 'x' }],
+        [{ insert: 'line 1' }, { insert: '\n' }, { insert: 'line 2' }],
+        [{ retain: 100 }]
+      ];
+
+      for (const v of values) {
+        it(`should return \`false\` for: ${inspect(v)}`, () => {
+          const delta = new BodyDelta(v);
+          assert.isFalse(delta.isEmpty());
+        });
+      }
+    });
+  });
+
+  describe('isDocument()', () => {
     describe('`true` cases', () => {
       const values = [
         [],
