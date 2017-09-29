d652a5731d1420dd9e5d6d80664d54618f321959
Dan Bornstein <danfuzz@slack-corp.com>
2017-09-29 16:17:34 -0700

    There's not much point to complaining about these coverage oversights.
    ...at least not right now.

--- local-modules/see-all-server/tests/test_FileSink.js
+++
@@ -1,9 +0,0 @@
-// Copyright 2016-2017 the Bayou Authors (Dan Bornstein et alia).
-// Licensed AS IS and WITHOUT WARRANTY under the Apache License,
-// Version 2.0. Details: <http://www.apache.org/licenses/LICENSE-2.0>
-
-import { describe, it } from 'mocha';
-
-describe('see-all-server/FileSink', () => {
-  it('needs a way to be tested');
-});
