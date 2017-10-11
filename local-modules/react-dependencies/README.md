3fdef462aa9a6be63fc968d6f450f33cf165c34c
Dan Bornstein <danfuzz@slack-corp.com>
2017-10-11 16:53:51 -0700

    Extract a `react-dependencies` module.
    To avoid having to keep individual module dependencies in synch.
    From @meantime.

---
+++ local-modules/react-dependencies/README.md
@@ -0,0 +1,5 @@
+react-dependencies
+=====================
+
+This module just serves as a single location to hold all of the react/redux
+dependencies.
---
+++ local-modules/react-dependencies/main.js
@@ -0,0 +1,7 @@
+// Copyright 2016-2017 the Bayou Authors (Dan Bornstein et alia).
+// Licensed AS IS and WITHOUT WARRANTY under the Apache License,
+// Version 2.0. Details: <http://www.apache.org/licenses/LICENSE-2.0>
+
+// This module serves only to hold common dependencies for react and
+// redux. There is nothing to export.
+module.exports = {};
