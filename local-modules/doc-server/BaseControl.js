9a31fa8a1a23b907983aa6f4156c7331e2066a81
Dan Bornstein <danfuzz@slack-corp.com>
2017-10-13 16:21:03 -0700

    Extract `BaseComplexMember`.
    ...because `CaretStorage` is part of the file complex, but it _isn't_ a
    controller.

--- local-modules/doc-server/BaseControl.js
+++ local-modules/doc-server/BaseControl.js
@@ -2,16 +2,14 @@
 // Licensed AS IS and WITHOUT WARRANTY under the Apache License,
 // Version 2.0. Details: <http://www.apache.org/licenses/LICENSE-2.0>
 
-import { CommonBase } from 'util-common';
-
-import FileComplex from './FileComplex';
+import BaseComplexMember from './BaseComplexMember';
 
 /**
  * Base class for document part controllers. There is one instance of each
  * concrete subclass of this class for each actively-edited document. They are
  * all managed and hooked up via {@link FileComplex}.
  */
-export default class BaseControl extends CommonBase {
+export default class BaseControl extends BaseComplexMember {
   /**
    * Constructs an instance.
    *
@@ -19,38 +17,9 @@ export default class BaseControl extends CommonBase {
    *   of.
    */
   constructor(fileComplex) {
-    super();
-
-    /** {FileComplex} File complex that this instance is part of. */
-    this._fileComplex = FileComplex.check(fileComplex);
-
-    /** {BaseFile} The underlying document storage. */
-    this._file = fileComplex.file;
-
-    /** {FileCodec} File-codec wrapper to use. */
-    this._fileCodec = fileComplex.fileCodec;
-
-    /** {Logger} Logger specific to this document's ID. */
-    this._log = fileComplex.log;
+    super(fileComplex);
   }
 
-  /** {BaseFile} The underlying document storage. */
-  get file() {
-    return this._file;
-  }
-
-  /** {FileCodec} File-codec wrapper to use when dealing with encoded data. */
-  get fileCodec() {
-    return this._fileCodec;
-  }
-
-  /** {FileComplex} File complex that this instance is part of. */
-  get fileComplex() {
-    return this._fileComplex;
-  }
-
-  /** {Logger} Logger to use with this instance. */
-  get log() {
-    return this._log;
-  }
+  // **TODO:** Eventually this class will have the base versions of things like
+  // `snapshot()` and `getChangeAfter()`.
 }
