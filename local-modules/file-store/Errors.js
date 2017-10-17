63704f309957a6201c2eb1794b732517b28455d7
Dan Bornstein <danfuzz@slack-corp.com>
2017-10-17 16:01:30 -0700

    Remove unneeded `import`.

--- local-modules/file-store/Errors.js
+++ local-modules/file-store/Errors.js
@@ -2,7 +2,7 @@
 // Licensed AS IS and WITHOUT WARRANTY under the Apache License,
 // Version 2.0. Details: <http://www.apache.org/licenses/LICENSE-2.0>
 
-import { TInt, TString } from 'typecheck';
+import { TString } from 'typecheck';
 import { FrozenBuffer, InfoError, UtilityClass } from 'util-common';
 
 import StoragePath from './StoragePath';
