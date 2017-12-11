63fef399e9790b2dd4b92a5514c64727312a0f29
Dan Bornstein <danfuzz@slack-corp.com>
2017-12-11 13:00:27 -0800

    Explain what's going on.

--- local-modules/doc-server/DurableControl.js
+++ local-modules/doc-server/DurableControl.js
@@ -7,6 +7,11 @@ import BaseControl from './BaseControl';
 /**
  * Base class for _durable_ document part controllers. Durable parts maintain
  * full change history (as opposed to _ephemeral_ parts which do not).
+ *
+ * **Note:** The actual differences in behavior between instances of this class
+ * and instances of {@link EphemeralControl} are defined in the superclass,
+ * which merely keys off of the value of the static property {@link #ephemeral}
+ * defined here.
  */
 export default class DurableControl extends BaseControl {
   /**
