cf772a2542073f9cbf55c6b0c08de6b5f060c092
Dan Bornstein <danfuzz@slack-corp.com>
2018-01-25 12:37:55 -0800

    Add new file ops `deletePath{Prefix,Range}`.

--- local-modules/file-store-ot/FileOp.js
+++ local-modules/file-store-ot/FileOp.js
@@ -3,6 +3,7 @@
 // Version 2.0. Details: <http://www.apache.org/licenses/LICENSE-2.0>
 
 import { BaseOp } from 'ot-common';
+import { TInt } from 'typecheck';
 import { Errors, FrozenBuffer } from 'util-common';
 
 import StorageId from './StorageId';
@@ -25,6 +26,16 @@ export default class FileOp extends BaseOp {
   /** {string} Opcode constant for "delete path" operations. */
   static get CODE_deletePath() {
     return 'deletePath';
+  }
+
+  /** {string} Opcode constant for "delete path prefix" operations. */
+  static get CODE_deletePathPrefix() {
+    return 'deletePathPrefix';
+  }
+
+  /** {string} Opcode constant for "delete path range" operations. */
+  static get CODE_deletePathRange() {
+    return 'deletePathRange';
   }
 
   /** {string} Opcode constant for "write blob" operations. */
@@ -65,9 +76,8 @@ export default class FileOp extends BaseOp {
 
   /**
    * Constructs a new "delete path" operation. The effect of this operation is
-   * to remove the blob stored at (exactly) the indicated path, if the path is
-   * in fact bound in the file. If it isn't bound, then the operation does
-   * nothing.
+   * to remove any value binding for the indicated path, if the path is in fact
+   * bound in the file. If it isn't bound, then the operation does nothing.
    *
    * @param {string} path Path to delete. Must be valid per {@link StoragePath}.
    * @returns {FileOp} An appropriately-constructed operation.
@@ -76,6 +86,48 @@ export default class FileOp extends BaseOp {
     StoragePath.check(path);
 
     return new FileOp(FileOp.CODE_deletePath, path);
+  }
+
+  /**
+   * Constructs a new "delete path prefix" operation. The effect of this
+   * operation is to remove all value bindings for the indicated path and for
+   * all bound paths which have the indicated path as a prefix, if any. If there
+   * are no such bound paths in the file, then this operation does nothing.
+   *
+   * @param {string} path Path prefix to delete. Must be valid per {@link
+   *   StoragePath}.
+   * @returns {FileOp} An appropriately-constructed operation.
+   */
+  static op_deletePathPrefix(path) {
+    StoragePath.check(path);
+
+    return new FileOp(FileOp.CODE_deletePathPrefix, path);
+  }
+
+  /**
+   * Constructs a new "delete path range" operation. The effect of this
+   * operation is to remove the value bindings for all paths immediately under
+   * the given path prefix whose final components are in the form of
+   * non-negative decimal numbers within the indicated range, if any. If there
+   * are no such bound paths in the file, then this operation does nothing.
+   *
+   * For example, `deletePathPrefix('/x/y', 20, 23)` would possibly affect the
+   * paths `/x/y/20`, `/x/y/21`, and `/x/y/22`; and no others.
+   *
+   * @param {string} path Path prefix for the values to delete. Must be valid
+   *   per {@link StoragePath}.
+   * @param {Int} startInclusive The start of the range to delete (inclusive).
+   *   Must be `>= 0`.
+   * @param {Int} endExclusive The end of the range to delete (exclusive). Must
+   *   be `> startInclusive`.
+   * @returns {FileOp} An appropriately-constructed operation.
+   */
+  static op_deletePathRange(path, startInclusive, endExclusive) {
+    StoragePath.check(path);
+    TInt.nonNegative(startInclusive);
+    TInt.min(endExclusive, startInclusive + 1);
+
+    return new FileOp(FileOp.CODE_deletePathRange, path, startInclusive, endExclusive);
   }
 
   /**
@@ -130,9 +182,15 @@ export default class FileOp extends BaseOp {
         return Object.freeze({ opName, hash });
       }
 
-      case FileOp.CODE_deletePath: {
+      case FileOp.CODE_deletePath:
+      case FileOp.CODE_deletePathPrefix: {
         const [path] = payload.args;
         return Object.freeze({ opName, path });
+      }
+
+      case FileOp.CODE_deletePathRange: {
+        const [path, minInclusive, maxExclusive] = payload.args;
+        return Object.freeze({ opName, path, minInclusive, maxExclusive });
       }
 
       case FileOp.CODE_writeBlob: {
