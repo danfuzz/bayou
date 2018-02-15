75d37884a822066685266b91dc4de78e77da8efa
Dan Bornstein <danfuzz@slack-corp.com>
2018-02-14 16:14:36 -0800

    Add new `.baseUrl` hook.

--- local-modules/hooks-server/Hooks.js
+++ local-modules/hooks-server/Hooks.js
@@ -17,6 +17,17 @@ import BearerTokens from './BearerTokens';
  */
 export default class Hooks extends Singleton {
   /**
+   * {string} The base URL to use when constructing URLs to point at the
+   * public-facing (set of) machine(s) which front for this server.
+   *
+   * The default value for this is `http://localhost:N` where `N` is the value
+   * of {@link #listenPort}.
+   */
+  get baseUrl() {
+    return `http://localhost:${this.listenPort}`;
+  }
+
+  /**
    * {BearerTokens} The object which validates and authorizes bearer tokens.
    * See that (base / default) class for details.
    */
