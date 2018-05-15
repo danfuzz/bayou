161a2b632e8c5091e7e892e5c021e197af5d2b40
Dan Bornstein <danfuzz@slack-corp.com>
2018-05-15 15:08:43 -0700

    Move the `testing-common` module under the `@bayou` namespace.

--- local-modules/testing-server/CollectingReporter.js
+++ local-modules/testing-server/CollectingReporter.js
@@ -2,7 +2,7 @@
 // Licensed AS IS and WITHOUT WARRANTY under the Apache License,
 // Version 2.0. Details: <http://www.apache.org/licenses/LICENSE-2.0>
 
-import { BaseReporter } from 'testing-common';
+import { BaseReporter } from '@bayou/testing-common';
 
 import TestCollector from './TestCollector';
 
