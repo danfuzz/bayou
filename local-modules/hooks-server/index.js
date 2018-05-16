c5fd898a4f46ebe7d90d71bd836519b23d69be4a
Dan Bornstein <danfuzz@slack-corp.com>
2018-05-16 11:55:37 -0700

    Move the big explanation to `hooks-common`.

--- local-modules/hooks-server/index.js
+++ local-modules/hooks-server/index.js
@@ -7,21 +7,8 @@
 // defined here can end up `import`ing other modules which in turn want to use
 // hooks, that is to say, they can cause a circular dependency.
 //
-// We break the circle by deferring loading of the real hooks until first
-// actual usage of a hook property (or method), at which point all of the
-// initial module loading should have all resolved.
-//
-// We choose this module _specifically_ (and its client-side counterpart) as
-// the locale to break the would-be cycle, because this module by its nature is
-// a dependency bottleneck _and_ tends to be loosely coupled with its
-// dependencies. So, (on the first front) breaking it here means there are many
-// fewer other places where we need to worry about this, and (on the second
-// front) this is a relatively un-precarious place to perform the breaking.
-//
-// **Note:** If you switch this to just doing `import Hooks`, then what will
-// ultimately happen is that some `import` will fail "soft" by returning
-// `undefined`. It is impossible to predict in advance _which_ `import`
-// will fail, though.
+// See the comment in `hooks-common/index.js` for a longer discussion about
+// this.
 
 import { DataUtil, DeferredLoader } from '@bayou/util-common';
 
