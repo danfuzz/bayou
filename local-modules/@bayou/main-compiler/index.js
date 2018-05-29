5936eabab62e35f174583d94a61fcd60393241c5
Dan Bornstein <danfuzz@slack-corp.com>
2018-05-29 13:36:49 -0700

    Rework the top `compiler` to be a local module.

--- local-modules/@bayou/main-compiler/index.js
+++ local-modules/@bayou/main-compiler/index.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 // Copyright 2016-2018 the Bayou Authors (Dan Bornstein et alia).
 // Licensed AS IS and WITHOUT WARRANTY under the Apache License,
 // Version 2.0. Details: <http://www.apache.org/licenses/LICENSE-2.0>
