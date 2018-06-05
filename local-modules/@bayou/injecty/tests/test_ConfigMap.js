94f36040f53c18ff47825f804664e174ac411a68
Dan Bornstein <danfuzz@slack-corp.com>
2018-06-05 13:31:16 -0700

    Add a small handful of unit tests.

---
+++ local-modules/@bayou/injecty/tests/test_ConfigMap.js
@@ -0,0 +1,41 @@
+// Copyright 2016-2018 the Bayou Authors (Dan Bornstein et alia).
+// Licensed AS IS and WITHOUT WARRANTY under the Apache License,
+// Version 2.0. Details: <http://www.apache.org/licenses/LICENSE-2.0>
+
+import { assert } from 'chai';
+import { describe, it } from 'mocha';
+
+// This class isn't exported publicly, so it needs to be imported by path.
+import ConfigMap from '@bayou/injecty/ConfigMap';
+
+describe('@bayou/injecty/ConfigMap', () => {
+  describe('constructor', () => {
+    it('constructs an instance without error', () => {
+      new ConfigMap();
+    });
+  });
+
+  describe('add()', () => {
+    it('throws an error when called twice with the same name', () => {
+      const cm = new ConfigMap();
+
+      cm.add('whee', 1);
+      assert.throws(() => { cm.add('whee', 1); }, /badUse/);
+    });
+  });
+
+  describe('get()', () => {
+    it('retrieves a previously-added value', () => {
+      const cm = new ConfigMap();
+      const v  = ['yay'];
+
+      cm.add('whee', v);
+      assert.strictEqual(cm.get('whee'), v);
+    });
+
+    it('throws an error when given an un-added name', () => {
+      const cm = new ConfigMap();
+      assert.throws(() => { cm.get('florp'); }, /badUse/);
+    });
+  });
+});
---
+++ local-modules/@bayou/injecty/tests/test_InjectHandler.js
@@ -0,0 +1,25 @@
+// Copyright 2016-2018 the Bayou Authors (Dan Bornstein et alia).
+// Licensed AS IS and WITHOUT WARRANTY under the Apache License,
+// Version 2.0. Details: <http://www.apache.org/licenses/LICENSE-2.0>
+
+import { assert } from 'chai';
+import { describe, it } from 'mocha';
+
+// These classes aren't exported publicly, so they need to be imported by path.
+import ConfigMap from '@bayou/injecty/ConfigMap';
+import InjectHandler from '@bayou/injecty/InjectHandler';
+
+// **Note:** These tests all use proxy instances in the way expected of clients
+// of this module.
+describe('@bayou/injecty/InjectHandler', () => {
+  it('stores through to a `ConfigMap` instance', () => {
+    const cm     = new ConfigMap();
+    const inject = InjectHandler.makeProxy(cm);
+
+    inject.foo = 'FOO';
+    inject.bar = 'BAR';
+
+    assert.strictEqual(cm.get('foo'), 'FOO');
+    assert.strictEqual(cm.get('bar'), 'BAR');
+  });
+});
