9c6bf1b3f35cef818f39f4cd86a63a02672995f6
Dan Bornstein <danfuzz@slack-corp.com>
2018-06-05 12:44:18 -0700

    First cut of a very simple dependency injection mechanism.

---
+++ local-modules/@bayou/injecty/AllConfigs.js
@@ -0,0 +1,25 @@
+// Copyright 2016-2018 the Bayou Authors (Dan Bornstein et alia).
+// Licensed AS IS and WITHOUT WARRANTY under the Apache License,
+// Version 2.0. Details: <http://www.apache.org/licenses/LICENSE-2.0>
+
+import { Singleton } from '@bayou/util-common';
+
+import ConfigMap from './ConfigMap';
+
+/**
+ * Module-internal singleton class which holds the system-wide instance of
+ * {@link ConfigMap}.
+ */
+export default class AllConfigs extends Singleton {
+  /**
+   * Constructs an instance.
+   */
+  constructor() {
+    super();
+
+    /** {ConfigMap} The configurations. */
+    this.map = new ConfigMap();
+
+    Object.freeze(this);
+  }
+}
---
+++ local-modules/@bayou/injecty/ConfigMap.js
@@ -0,0 +1,58 @@
+// Copyright 2016-2018 the Bayou Authors (Dan Bornstein et alia).
+// Licensed AS IS and WITHOUT WARRANTY under the Apache License,
+// Version 2.0. Details: <http://www.apache.org/licenses/LICENSE-2.0>
+
+import { TString } from '@bayou/typecheck';
+import { CommonBase, Errors } from '@bayou/util-common';
+
+/**
+ * Map of names to injected configurations.
+ */
+export default class Configuration extends CommonBase {
+  /**
+   * Constructs an instance.
+   */
+  constructor() {
+    super();
+
+    /** {Map<string,*>} All injected configurations. */
+    this._items = new Map();
+  }
+
+  /**
+   * Adds a new configured object or value. It is an error to call this with the
+   * same name more than once.
+   *
+   * @param {string} name Name of the thing to inject. Must be an "identifier."
+   * @param {*} value Value to associate with `name`.
+   */
+  add(name, value) {
+    TString.identifier(name);
+
+    if (this._items.has(name)) {
+      throw Errors.badUse(`Name already configured: ${name}`);
+    }
+
+    this._items.set(name, value);
+  }
+
+  /**
+   * Gets the previously-added configured object or value associated with the
+   * given name. It is an error to pass a name which was not previously
+   * configured via {@link #add}.
+   *
+   * @param {string} name Name of the previously-injected configuration.
+   * @returns {*} Associated value or object.
+   */
+  get(name) {
+    TString.identifier(name);
+
+    const result = this._items.get(name);
+
+    if ((result === undefined) && !this._items.has(name)) {
+      throw Errors.badUse(`Name not configured: ${name}`);
+    }
+
+    return result;
+  }
+}
