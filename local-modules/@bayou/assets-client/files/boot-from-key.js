5fe7ec560c1908c6cf9fec1745c37a094e590658
Dan Bornstein <danfuzz@slack-corp.com>
2018-06-13 15:13:12 -0700

    Remove a now-unneeded directory layer.
    Also, clean up some comments.

--- local-modules/@bayou/assets-client/files/boot-from-key.js
+++ local-modules/@bayou/assets-client/files/boot-from-key.js
@@ -22,21 +22,24 @@
 // polluting the global namespace.
 (function () {
   if (!(window.BAYOU_KEY && window.BAYOU_NODE)) {
-    // **Note:** This code is run too early to be able to use `@bayou/util-common`'s
-    // error facilities.
+    // **Note:** This code is run too early to be able to use
+    // `@bayou/util-common`'s error facilities.
     throw new Error('Missing configuration.');
   }
 
   // Grab the base URL out of the encoded key. This is kinda gross, but when
   // we're here we haven't yet loaded the API code, and in order to load that
   // code we need to know the base URL, whee! So we just do the minimal bit of
-  // parsing needed to get the URL and then head on our merry way.
+  // parsing needed to get the URL and then head on our merry way. See
+  // {@link @bayou/api-common/SplitKey}, the encoded form in particular, if you
+  // want to understand what's going on.
   var key = JSON.parse(window.BAYOU_KEY);
-  var url = key.SplitKey[0]; // See `SplitKey.js`, the encoded form in particular.
+  var url = key.SplitKey[0];
   var baseUrl = ((url === '*') ? window.location : new URL(url)).origin;
 
   // Add the main JavaScript bundle to the page. Once loaded, this continues
-  // the boot process. You can find its main entrypoint in `client/js/index.js`.
+  // the boot process. You can find its main entrypoint in
+  // {@link @bayou/main-client} listed as the `main` in that module's manifest.
   var elem = document.createElement('script');
   elem.src = baseUrl + '/static/js/main.bundle.js';
   document.head.appendChild(elem);
