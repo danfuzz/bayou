57a144c1fbf29ada8e8c4248534e84625ecae8cd
Dan Bornstein <danfuzz@slack-corp.com>
2018-06-28 11:02:14 -0700

    Move `BayouKeyboard` to `doc-ui`.

--- local-modules/@bayou/quill-util/index.js
+++ local-modules/@bayou/quill-util/index.js
@@ -2,22 +2,13 @@
 // Licensed AS IS and WITHOUT WARRANTY under the Apache License,
 // Version 2.0. Details: <http://www.apache.org/licenses/LICENSE-2.0>
 
-import BayouKeyboard from './BayouKeyboard';
 import NotReallyMarkdown from './NotReallyMarkdown';
 import QuillEvents from './QuillEvents';
 import QuillGeometry from './QuillGeometry';
 import QuillProm from './QuillProm';
 import QuillUtil from './QuillUtil';
 
-// Register this module's keyboard handler as an override of Quill's built-in
-// one.
-QuillProm.register({
-  'modules/keyboard': BayouKeyboard
-}, true);
-
-
 export {
-  BayouKeyboard,
   NotReallyMarkdown,
   QuillEvents,
   QuillGeometry,
