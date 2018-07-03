16123305ad59b6bab6c60637f10aee38487c5808
Dan Bornstein <danfuzz@slack-corp.com>
2018-07-03 13:13:03 -0600

    Remove unused class `NotReallyMarkdown`.
    If we want to do mardown-ish key gestures, we may revive this class or
    perhaps do it another way.

--- local-modules/@bayou/quill-util/index.js
+++ local-modules/@bayou/quill-util/index.js
@@ -2,14 +2,12 @@
 // Licensed AS IS and WITHOUT WARRANTY under the Apache License,
 // Version 2.0. Details: <http://www.apache.org/licenses/LICENSE-2.0>
 
-import NotReallyMarkdown from './NotReallyMarkdown';
 import QuillEvents from './QuillEvents';
 import QuillGeometry from './QuillGeometry';
 import QuillProm from './QuillProm';
 import QuillUtil from './QuillUtil';
 
 export {
-  NotReallyMarkdown,
   QuillEvents,
   QuillGeometry,
   QuillProm,
