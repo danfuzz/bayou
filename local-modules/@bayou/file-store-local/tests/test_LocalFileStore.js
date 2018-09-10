0618347a3e8079b2de5e8b24b97ca1fd2523d55a
Dan Bornstein <danfuzz@slack-corp.com>
2018-09-10 16:24:44 -0700

    Remove redundant `describe()`.

--- local-modules/@bayou/file-store-local/tests/test_LocalFileStore.js
+++ local-modules/@bayou/file-store-local/tests/test_LocalFileStore.js
@@ -18,22 +18,20 @@ describe('@bayou/file-store-local/LocalFileStore', () => {
   });
 
   describe('isFileId()', () => {
-    describe('isFileId()', () => {
-      it('should accept 32-character alphanum ASCII strings', () => {
-        assert.isTrue(lfs.isFileId('123abc7890ABC456789012'));
-      });
+    it('should accept 32-character alphanum ASCII strings', () => {
+      assert.isTrue(lfs.isFileId('123abc7890ABC456789012'));
+    });
 
-      it('should allow underscores and hyphens', () => {
-        assert.isTrue(lfs.isFileId('123456789_123456789-12'));
-      });
+    it('should allow underscores and hyphens', () => {
+      assert.isTrue(lfs.isFileId('123456789_123456789-12'));
+    });
 
-      it('should not allow non-ASCII characters', () => {
-        assert.isFalse(lfs.isFileId('123456789•123456789•12'));
-      });
+    it('should not allow non-ASCII characters', () => {
+      assert.isFalse(lfs.isFileId('123456789•123456789•12'));
+    });
 
-      it('should not allow non-alphanum characters', () => {
-        assert.isFalse(lfs.isFileId('123456789\t123456789+12'));
-      });
+    it('should not allow non-alphanum characters', () => {
+      assert.isFalse(lfs.isFileId('123456789\t123456789+12'));
     });
   });
 });
