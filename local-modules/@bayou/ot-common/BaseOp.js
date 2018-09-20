f123149648fe442738e0f08f3fe0f86ac1459920
Dan Bornstein <danfuzz@slack-corp.com>
2018-09-20 11:42:21 -0700

    Add validation for operation names.
    This is done programatically based on the `CODE_*` properties defined by
    each concrete subclass of `BaseOp`.

--- local-modules/@bayou/ot-common/BaseOp.js
+++ local-modules/@bayou/ot-common/BaseOp.js
@@ -2,14 +2,89 @@
 // Licensed AS IS and WITHOUT WARRANTY under the Apache License,
 // Version 2.0. Details: <http://www.apache.org/licenses/LICENSE-2.0>
 
-import { CommonBase, Functor } from '@bayou/util-common';
+import { TBoolean, TString } from '@bayou/typecheck';
+import { CommonBase, Errors, Functor } from '@bayou/util-common';
 
 /**
  * Base class for OT operations. Instances of concrete subclasses of this class
  * are what compose the main contents of a corresponding concrete {@link
  * BaseDelta} subclasses.
+ *
+ * **Note:** Each concrete subclass of this class needs to define a set of
+ * static properties with names of the form `CODE_<name>`, each of which has a
+ * string value. These values are collectively taken to be the acceptable opcode
+ * names for use with the concrete subclass.
  */
 export default class BaseOp extends CommonBase {
+  /**
+   * Indicates whether the given name is acceptable for use as an opcode name
+   * on an instance of this class.
+   *
+   * **Note:** This depends on the set of `CODE_*` properties being correct for
+   * the concrete subclass class.
+   *
+   * @param {string} name Potential opcode name.
+   * @returns {boolean} `true` if `name` is valid for use as an opcode name on
+   *   this class, or `false` if not.
+   */
+  static isValidName(name) {
+    TString.check(name);
+
+    if (!this._names) {
+      // First time this method has been called on the concrete subclass;
+      // collect all the names.
+
+      const names = new Set();
+      const descs = Object.getOwnPropertyDescriptors(this);
+
+      for (const [propName, desc] of Object.entries(descs)) {
+        if (!/^CODE_/.test(propName)) {
+          continue;
+        }
+
+        const value = TString.nonEmpty(desc.get ? desc.get() : desc.value);
+        names.add(value);
+      }
+
+      this._names = Object.freeze(names);
+    }
+
+    return this._names.has(name);
+  }
+
+  /**
+   * Validates a {@link Functor} to be used as the payload for an instance of
+   * this class, returning a `boolean` indicating validity.
+   *
+   * @param {Functor} payload The would-be payload for an instance.
+   * @returns {boolean} `true` if `payload` is valid, or `false` if not.
+   */
+  static isValidPayload(payload) {
+    if (!this.isValidName(payload.name)) {
+      return false;
+    }
+
+    const result = this._impl_isValidPayload(payload);
+
+    return TBoolean.check(result);
+  }
+
+  /**
+   * Validates a {@link Functor} to be used as the payload for an instance of
+   * this class.
+   *
+   * @param {Functor} payload The would-be payload for an instance.
+   * @returns {Functor} `payload`, if it turns out to be valid.
+   * @throws {Error} Thrown if `payload` is invalid.
+   */
+  static validatePayload(payload) {
+    if (this.isValidPayload(payload)) {
+      return payload;
+    }
+
+    throw Errors.badUse(`Invalid payload for ${this.name}.`);
+  }
+
   /**
    * Constructs an instance. This should not be used directly. Instead, use
    * the static constructor methods defined by concrete subclasses of this
@@ -24,11 +99,12 @@ export default class BaseOp extends CommonBase {
   constructor(name, ...args) {
     super();
 
-    // Perform syntactic validation based on subclass
-    this._impl_validate(name, args);
+    // Perform syntactic validation based on the concrete subclass.
+    const payload = new Functor(name, ...args).withFrozenArgs();
+    this.constructor.validatePayload(payload);
 
     /** {Functor} The operation payload (name and arguments). */
-    this._payload = new Functor(name, ...args).withFrozenArgs();
+    this._payload = payload;
 
     Object.freeze(this);
   }
@@ -70,16 +146,14 @@ export default class BaseOp extends CommonBase {
   }
 
   /**
-   * Abstract function to alidates op arguments based on
-   * op subclass. Subclasses must implement their own validation.
+   * Main implementation of {@link #isValidPayload}. Subclasses must fill this
+   * in.
    *
-   * @param {string} name The name of the op type.
-   * @param {array} args The op arguments to validate.
-   * @returns {boolean} `true` if arguments are valid,
-   *   throws and error otherwise.
    * @abstract
+   * @param {Functor} payload The would-be payload for an instance.
+   * @returns {boolean} `true` if `payload` is valid, or `false` if not.
    */
-  _impl_validate(name, args) {
-    return this._mustOverride(name, args);
+  static _impl_isValidPayload(payload) {
+    return this._mustOverride(payload);
   }
 }
