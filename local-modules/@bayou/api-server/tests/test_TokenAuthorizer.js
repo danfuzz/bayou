9ad75116f30777d7671f80506587e61809036b16
Dan Bornstein <danfuzz@slack-corp.com>
2018-10-22 15:01:40 -0600

    Add more tests.

--- local-modules/@bayou/api-server/tests/test_TokenAuthorizer.js
+++ local-modules/@bayou/api-server/tests/test_TokenAuthorizer.js
@@ -29,4 +29,43 @@ describe('@bayou/api-server/TokenAuthorizer', () => {
       assert.throws(() => new Authie().nonTokenPrefix, /badValue/);
     });
   });
+
+  describe('isToken()', () => {
+    it('should call through to the `_impl` given a string', () => {
+      class Authie extends TokenAuthorizer {
+        _impl_isToken(value) {
+          return value.startsWith('token-');
+        }
+      }
+
+      const au = new Authie();
+
+      assert.isTrue(au.isToken('token-yes'));
+      assert.isFalse(au.isToken('not-a-token'));
+    });
+
+    it('should reject a non-string without calling through to the `_impl`', () => {
+      class Authie extends TokenAuthorizer {
+        _impl_isToken(value_unused) {
+          throw new Error('Should not have been called.');
+        }
+      }
+
+      const au = new Authie();
+
+      assert.throws(() => au.isToken(null), /badValue/);
+      assert.throws(() => au.isToken(123), /badValue/);
+      assert.throws(() => au.isToken(['x']), /badValue/);
+    });
+
+    it('should reject a bad subclass implementation', () => {
+      class Authie extends TokenAuthorizer {
+        get _impl_nonTokenPrefix() {
+          return ['not just a string'];
+        }
+      }
+
+      assert.throws(() => new Authie().nonTokenPrefix, /badValue/);
+    });
+  });
 });
