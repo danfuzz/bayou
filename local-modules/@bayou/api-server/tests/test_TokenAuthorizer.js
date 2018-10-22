c6adc18bf944230e3e461bc612947b35cbb6e4a8
Dan Bornstein <danfuzz@slack-corp.com>
2018-10-22 15:15:54 -0600

    Add more tests.

--- local-modules/@bayou/api-server/tests/test_TokenAuthorizer.js
+++ local-modules/@bayou/api-server/tests/test_TokenAuthorizer.js
@@ -5,6 +5,7 @@
 import { assert } from 'chai';
 import { describe, it } from 'mocha';
 
+import { BearerToken } from '@bayou/api-common';
 import { TokenAuthorizer } from '@bayou/api-server';
 
 describe('@bayou/api-server/TokenAuthorizer', () => {
@@ -60,12 +61,91 @@ describe('@bayou/api-server/TokenAuthorizer', () => {
 
     it('should reject a bad subclass implementation', () => {
       class Authie extends TokenAuthorizer {
-        get _impl_nonTokenPrefix() {
-          return ['not just a string'];
+        _impl_isToken(value_unused) {
+          return 'this is not a boolean';
         }
       }
 
-      assert.throws(() => new Authie().nonTokenPrefix, /badValue/);
+      assert.throws(() => new Authie().isToken('x'), /badValue/);
+    });
+  });
+
+  describe('targetFromToken()', () => {
+    it('should call through to the `_impl` given a `BearerToken`', async () => {
+      class Authie extends TokenAuthorizer {
+        async _impl_targetFromToken(value) {
+          return { got: value };
+        }
+      }
+
+      const au     = new Authie();
+      const token  = new BearerToken('x', 'y');
+      const result = await au.targetFromToken(token);
+
+      assert.deepEqual(result, { got: token });
+    });
+
+    it('should convert a string to a `BearerToken` then through to the `_impl`', async () => {
+      class Authie extends TokenAuthorizer {
+        async _impl_targetFromToken(value) {
+          return { got: value };
+        }
+
+        _impl_tokenFromString(value) {
+          return new BearerToken(value, value);
+        }
+
+        _impl_isToken(value_unused) {
+          return true;
+        }
+      }
+
+      const au     = new Authie();
+      const result = await au.targetFromToken('yes');
+
+      assert.deepEqual(result, { got: new BearerToken('yes', 'yes') });
+    });
+
+    it('should reject a non-string non-`BearerToken` argument without calling through to the `_impl`', async () => {
+      class Authie extends TokenAuthorizer {
+        _impl_targetFromToken(value_unused) {
+          throw new Error('Should not have been called.');
+        }
+      }
+
+      const au = new Authie();
+
+      await assert.isRejected(au.targetFromToken(undefined), /badValue/);
+      await assert.isRejected(au.targetFromToken(null), /badValue/);
+      await assert.isRejected(au.targetFromToken(true), /badValue/);
+      await assert.isRejected(au.targetFromToken(914), /badValue/);
+      await assert.isRejected(au.targetFromToken(['foo']), /badValue/);
+    });
+
+    it('should accept `null` from the `_impl`', async () => {
+      class Authie extends TokenAuthorizer {
+        async _impl_targetFromToken(value_unused) {
+          return null;
+        }
+      }
+
+      const au     = new Authie();
+      const result = await au.targetFromToken(new BearerToken('x', 'y'));
+
+      assert.isNull(result);
+    });
+
+    it('should reject a bad subclass implementation', () => {
+      class Authie extends TokenAuthorizer {
+        _impl_targetFromToken(value_unused) {
+          // Supposed to be an object or `null`.
+          return 123;
+        }
+      }
+
+      const token = new BearerToken('x', 'y');
+
+      assert.isRejected(new Authie().targetFromToken(token), /badValue/);
     });
   });
 });
