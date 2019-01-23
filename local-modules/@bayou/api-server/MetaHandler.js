8a5cb86657aa8014f5c6b6e1f5da1dc05def9679
Dan Bornstein <danfuzz@slack-corp.com>
2019-01-23 11:47:26 -0800

    Remove challenge-response stuff from `MetaHandler`.

--- local-modules/@bayou/api-server/MetaHandler.js
+++ local-modules/@bayou/api-server/MetaHandler.js
@@ -2,13 +2,6 @@
 // Licensed AS IS and WITHOUT WARRANTY under the Apache License,
 // Version 2.0. Details: <http://www.apache.org/licenses/LICENSE-2.0>
 
-import { TString } from '@bayou/typecheck';
-import { Delay } from '@bayou/promise-util';
-import { Errors } from '@bayou/util-common';
-
-/** {Int} How long an unanswered challenge remains active for, in msec. */
-const CHALLENGE_TIMEOUT_MSEC = 5 * 60 * 1000; // Five minutes.
-
 /**
  * Class to handle meta-requests.
  */
@@ -24,52 +17,6 @@ export default class MetaHandler {
 
     /** {Logger} The connection-specific logger. */
     this._log = connection.log;
-
-    /**
-     * {Map<challenge, {id, response}>} Map from challenge string to associated
-     * ID and response.
-     */
-    this._activeChallenges = new Map();
-  }
-
-  /**
-   * Called to respond to a successful challenge (which was presumably made via
-   * `makeChallenge()`. If `challenge` and `response` correspond to an active
-   * challenge-response pair, then this method will remove the key that controls
-   * the associated target, in the current session. (Other sessions will not be
-   * affected, including sessions created later.) It is only ever valid to
-   * respond to any given challenge once.
-   *
-   * This method will throw an error if the arguments don't refer to an active
-   * challenge (because it never existed, because it was already used, or
-   * because it expired).
-   *
-   * @param {string} challenge The challenge string.
-   * @param {string} response The challenge response.
-   */
-  async authWithChallengeResponse(challenge, response) {
-    TString.check(challenge);
-    TString.check(response);
-
-    const challengeInfo = this._activeChallenges.get(challenge);
-
-    if (!challengeInfo || (response !== challengeInfo.response)) {
-      // **Note:** We don't differentiate reasons for rejection (beyond type
-      // checking, above), as that could reveal security-sensitive info.
-      throw Errors.badUse('Invalid challenge pair.');
-    }
-
-    const id = challengeInfo.id;
-
-    // Remove the challenge from the active set, so that a given challenge can
-    // only be used once.
-    this._activeChallenges.delete(challenge);
-
-    // The main action: Replace the target for `id` with an equivalent one
-    // except without auth control.
-    await this._connection.context.removeControl(id);
-
-    this._log.event.authedChallenge(id, challenge);
   }
 
   /**
@@ -81,54 +28,6 @@ export default class MetaHandler {
    */
   connectionId() {
     return this._connection.connectionId;
-  }
-
-  /**
-   * Generates and returns a challenge for the key that controls the target with
-   * the given ID. It is an error to request a challenge for a nonexistent or
-   * uncontrolled target. Once returned, challenges are considered active only
-   * for a limited amount of time (approximately five minutes), after which
-   * they are expired.
-   *
-   * @param {string} id Target whose key is to be challenged.
-   * @returns {string} A challenge string that can subsequently be answered so
-   *   as to remove key control for the target, in the context of the current
-   *   session.
-   */
-  async makeChallenge(id) {
-    const target = await this._connection.context.getControlled(id);
-
-    let challengePair;
-    for (;;) {
-      challengePair = target.key.makeChallengePair();
-      if (!this._activeChallenges.get(challengePair.challenge)) {
-        break;
-      }
-
-      // We managed to get a duplicate challenge. This is highly unusual, but
-      // it _can_ happen. So, just iterate and try again.
-    }
-
-    const { challenge, response } = challengePair;
-
-    // Store the challenge, and arrange for its expiry.
-
-    this._activeChallenges.set(challenge, { id, response });
-
-    (async () => {
-      await Delay.resolve(CHALLENGE_TIMEOUT_MSEC);
-      if (this._activeChallenges.delete(challenge)) {
-        // `delete` returns `true` to indicate that the value was found. In this
-        // case, it means that it expired.
-        this._log.event.challengeExpired(id, challenge);
-      }
-    })();
-
-    // **Note:** It's probably okay to log the expected response, but may be
-    // worth thinking a bit more about. (Attention: Security professionals!)
-    this._log.event.newChallenge(id, challenge, response);
-
-    return challenge;
   }
 
   /**
