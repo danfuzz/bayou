1dc2ceb3cda23322839a01856523ef3390331567
Dan Bornstein <danfuzz@slack-corp.com>
2019-01-24 15:07:48 -0800

    Extract the (typically) fixed constructor args into a struct-ish class.

---
+++ local-modules/@bayou/api-server/ContextInfo.js
@@ -0,0 +1,48 @@
+// Copyright 2016-2019 the Bayou Authors (Dan Bornstein et alia).
+// Licensed AS IS and WITHOUT WARRANTY under the Apache License,
+// Version 2.0. Details: <http://www.apache.org/licenses/LICENSE-2.0>
+
+import { Codec } from '@bayou/codec';
+import { CommonBase } from '@bayou/util-common';
+
+import TokenAuthorizer from './TokenAuthorizer';
+
+/**
+ * All the info needed to construct instances of {@link Context}, except for
+ * logging-related stuff (which tends to be different for every instance).
+ */
+export default class ContextInfo extends CommonBase {
+  /**
+   * Constructs an instance.
+   *
+   * @param {Codec} codec Codec to use for all connections / sessions.
+   * @param {TokenAuthorizer|null} [tokenAuthorizer = null] Optional authorizer
+   *   for bearer tokens. If non-`null`, this is used to map bearer tokens into
+   *   usable target objects.
+   */
+  constructor(codec, tokenAuthorizer = null) {
+    super();
+
+    /** {Codec} The codec to use for connections / sessions. */
+    this._codec = Codec.check(codec);
+
+    /**
+     * {TokenAuthorizer|null} If non-`null`, authorizer to use in order to
+     * translate bearer tokens to target objects.
+     */
+    this._tokenAuthorizer =
+      (tokenAuthorizer === null) ? null : TokenAuthorizer.check(tokenAuthorizer);
+
+    Object.freeze(this);
+  }
+
+  /** {Codec} The codec to use for connections / sessions. */
+  get codec() {
+    return this._codec;
+  }
+
+  /** {TokenAuthorizer|null} The token authorizer to use. */
+  get tokenAuthorizer() {
+    return this._tokenAuthorizer;
+  }
+}
---
+++ local-modules/@bayou/api-server/tests/test_ContextInfo.js
@@ -0,0 +1,58 @@
+// Copyright 2016-2019 the Bayou Authors (Dan Bornstein et alia).
+// Licensed AS IS and WITHOUT WARRANTY under the Apache License,
+// Version 2.0. Details: <http://www.apache.org/licenses/LICENSE-2.0>
+
+import { assert } from 'chai';
+import { describe, it } from 'mocha';
+
+import { ContextInfo, TokenAuthorizer } from '@bayou/api-server';
+import { Codec } from '@bayou/codec';
+
+describe('@bayou/api-server/ContextInfo', () => {
+  describe('constructor()', () => {
+    it('accepts a single valid argument and produces a frozen instance', () => {
+      const result = new ContextInfo(new Codec());
+      assert.isFrozen(result);
+    });
+
+    it('accepts two valid arguments and produces a frozen instance', () => {
+      const result = new ContextInfo(new Codec(), new TokenAuthorizer());
+      assert.isFrozen(result);
+    });
+
+    it('accepts `null` as the second argument', () => {
+      const result = new ContextInfo(new Codec(), null);
+      assert.isFrozen(result);
+    });
+  });
+
+  describe('.codec', () => {
+    it('is the value passed into the constructor', () => {
+      const codec = new Codec();
+      const ci    = new ContextInfo(codec);
+
+      assert.strictEqual(ci.codec, codec);
+    });
+  });
+
+  describe('.tokenAuthorizer', () => {
+    it('is the non-`null` value passed into the constructor', () => {
+      const ta = new TokenAuthorizer();
+      const ci = new ContextInfo(new Codec(), ta);
+
+      assert.strictEqual(ci.tokenAuthorizer, ta);
+    });
+
+    it('is `null` if passed into the constructor as such', () => {
+      const ci = new ContextInfo(new Codec(), null);
+
+      assert.isNull(ci.tokenAuthorizer);
+    });
+
+    it('is `null` if omitted from the constructor', () => {
+      const ci = new ContextInfo(new Codec());
+
+      assert.isNull(ci.tokenAuthorizer);
+    });
+  });
+});
