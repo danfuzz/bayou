05e4629157e902517e18359735f350e1fed7a388
Dan Bornstein <danfuzz@slack-corp.com>
2019-02-05 14:35:54 -0800

    Add `makeFunctionProxy()`.

--- local-modules/@bayou/util-common/tests/test_BaseProxyHandler.js
+++ local-modules/@bayou/util-common/tests/test_BaseProxyHandler.js
@@ -8,8 +8,57 @@ import { describe, it } from 'mocha';
 import { BaseProxyHandler } from '@bayou/util-common';
 
 describe('@bayou/util-common/BaseProxyHandler', () => {
+  describe('makeFunctionProxy()', () => {
+    it('constructs a function-like proxy around an instance of the called-upon subclass', () => {
+      let gotArgs     = null;
+      let gotProperty = null;
+      let gotTarget   = null;
+      let gotThis     = null;
+
+      class Subclass extends BaseProxyHandler {
+        constructor(...args) {
+          super();
+          gotArgs = args;
+        }
+
+        apply(target, thisArg, args) {
+          gotTarget = target;
+          gotThis   = thisArg;
+          gotArgs   = args;
+
+          return 'boop';
+        }
+
+        get(target, property, receiver_unused) {
+          gotTarget   = target;
+          gotProperty = property;
+        }
+      }
+
+      const proxy = Subclass.makeFunctionProxy('x', 'y', 'z');
+
+      assert.deepEqual(gotArgs, ['x', 'y', 'z']);
+
+      assert.isUndefined(proxy.florp);
+      assert.frozen(gotTarget);
+      assert.isFunction(gotTarget);
+      assert.strictEqual(gotProperty, 'florp');
+
+      const callResult = proxy(1, 2, 3);
+      assert.strictEqual(callResult, 'boop');
+      assert.isFunction(gotTarget);
+      assert.isUndefined(gotThis);
+      assert.deepEqual(gotArgs, [1, 2, 3]);
+
+      const someThis = { yes: 'yes', p: proxy };
+      someThis.p('xyz');
+      assert.strictEqual(gotThis, someThis);
+      assert.deepEqual(gotArgs, ['xyz']);
+    });
+  });
+
   describe('makeProxy()', () => {
-    it('should construct a proxy around an instance of the called-upon subclass', () => {
+    it('constructs a proxy around an instance of the called-upon subclass', () => {
       let gotArgs = null;
       let gotTarget = null;
       let gotProperty = null;
@@ -38,13 +87,13 @@ describe('@bayou/util-common/BaseProxyHandler', () => {
   });
 
   describe('constructor', () => {
-    it('should construct an instance', () => {
+    it('constructs an instance', () => {
       assert.doesNotThrow(() => new BaseProxyHandler());
     });
   });
 
   describe('apply()', () => {
-    it('should always throw', () => {
+    it('always throws', () => {
       const func = () => { throw new Error('should not have been called'); };
       const bph = new BaseProxyHandler();
       const proxy = new Proxy(func, bph);
@@ -53,7 +102,7 @@ describe('@bayou/util-common/BaseProxyHandler', () => {
   });
 
   describe('construct()', () => {
-    it('should always throw', () => {
+    it('always throws', () => {
       const func = () => { throw new Error('should not have been called'); };
       const bph = new BaseProxyHandler();
       const proxy = new Proxy(func, bph);
@@ -62,21 +111,21 @@ describe('@bayou/util-common/BaseProxyHandler', () => {
   });
 
   describe('defineProperty()', () => {
-    it('should always return `false`', () => {
+    it('always returns `false`', () => {
       const bph = new BaseProxyHandler();
       assert.isFalse(bph.defineProperty({}, 'blort', { value: 123 }));
     });
   });
 
   describe('deleteProperty()', () => {
-    it('should always return `false`', () => {
+    it('always returns `false`', () => {
       const bph = new BaseProxyHandler();
       assert.isFalse(bph.deleteProperty({ blort: 10 }, 'blort'));
     });
   });
 
   describe('get()', () => {
-    it('should always return `undefined`', () => {
+    it('always returns `undefined`', () => {
       const bph = new BaseProxyHandler();
 
       assert.isUndefined(bph.get({}, 'x', {}));
@@ -85,14 +134,14 @@ describe('@bayou/util-common/BaseProxyHandler', () => {
   });
 
   describe('getOwnPropertyDescriptor()', () => {
-    it('should always throw', () => {
+    it('always throws', () => {
       const bph = new BaseProxyHandler();
       assert.throws(() => bph.getOwnPropertyDescriptor({ blort: 123 }, 'blort'));
     });
   });
 
   describe('getPrototypeOf()', () => {
-    it('should return the target\'s prototype', () => {
+    it('returns the target\'s prototype', () => {
       const bph = new BaseProxyHandler();
       const obj = new Map();
       assert.strictEqual(bph.getPrototypeOf(obj), Object.getPrototypeOf(obj));
@@ -100,35 +149,35 @@ describe('@bayou/util-common/BaseProxyHandler', () => {
   });
 
   describe('has()', () => {
-    it('should always return `false`', () => {
+    it('always returns `false`', () => {
       const bph = new BaseProxyHandler();
       assert.isFalse(bph.has({ blort: 10 }, 'blort'));
     });
   });
 
   describe('isExtensible()', () => {
-    it('should always return `false`', () => {
+    it('always returns `false`', () => {
       const bph = new BaseProxyHandler();
       assert.isFalse(bph.isExtensible({}));
     });
   });
 
   describe('ownKeys()', () => {
-    it('should always return `[]`', () => {
+    it('always returns `[]`', () => {
       const bph = new BaseProxyHandler();
       assert.deepEqual(bph.ownKeys({ a: 10, b: 20 }), []);
     });
   });
 
   describe('preventExstensions()', () => {
-    it('should always return `true`', () => {
+    it('always returns `true`', () => {
       const bph = new BaseProxyHandler();
       assert.isTrue(bph.preventExtensions({}));
     });
   });
 
   describe('set()', () => {
-    it('should always return `false`', () => {
+    it('always returns `false`', () => {
       const func = () => { throw new Error('should not have been called'); };
       const bph = new BaseProxyHandler();
       const proxy = new Proxy(func, bph);
@@ -137,7 +186,7 @@ describe('@bayou/util-common/BaseProxyHandler', () => {
   });
 
   describe('setPrototypeOf()', () => {
-    it('should always return `false`', () => {
+    it('always returns `false`', () => {
       const bph = new BaseProxyHandler();
 
       assert.isFalse(bph.setPrototypeOf({}, null));
