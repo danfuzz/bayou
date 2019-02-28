1bfb239a3de82a1637a2594ac3ca6e3eacddee4a
Dan Bornstein <danfuzz@slack-corp.com>
2019-02-27 16:46:30 -0800

    Transform strings to tokens on incoming messages.

--- local-modules/@bayou/api-server/BaseConnection.js
+++ local-modules/@bayou/api-server/BaseConnection.js
@@ -278,6 +278,15 @@ export default class BaseConnection extends CommonBase {
     }
 
     if (msg instanceof Message) {
+      const auth     = this._context.tokenAuthorizer;
+      const targetId = msg.targetId;
+
+      if (auth.isToken(targetId)) {
+        // The `targetId` is a token string. Replace the string with an actual
+        // `BearerToken` instance.
+        return msg.withTargetId(auth.tokenFromString(targetId));
+      }
+
       return msg;
     }
 
