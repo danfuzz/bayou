9ae1ca2a9fc30b706f9d67add8812efcde785489
Dan Bornstein <danfuzz@slack-corp.com>
2019-03-14 13:23:08 -0700

    A test that doesn't make sense if `isEmpty()` isn't special-cased.
    ...which it's not.

--- local-modules/@bayou/ot-common/tests/test_BaseSnapshot.js
+++ local-modules/@bayou/ot-common/tests/test_BaseSnapshot.js
@@ -171,14 +171,6 @@ describe('@bayou/ot-common/BaseSnapshot', () => {
         [new MockOp('composedDoc', 2), new MockOp('z')]);
     });
 
-    it('returns a value equal to `this` given same-`revNum` empty-`delta` changes', async () => {
-      const snap   = new MockSnapshot(10, [new MockOp('x')]);
-      const change = new MockChange(10, []);
-      const result = await snap.composeAll([change, change, change, change]);
-
-      assert.deepEqual(result, snap);
-    });
-
     it('calls the `yieldFunction` the appropriate number of times and with the expected arguments', async () => {
       const snap       = new MockSnapshot(10, [new MockOp('x')]);
       const allChanges = [];
