6b432bf4425f81decdb8bec6050266d15c4c3475
Dan Bornstein <danfuzz@slack-corp.com>
2019-04-16 16:52:22 -0700

    Remove unnecessary `import`.

--- local-modules/@bayou/see-all/LogProxyHandler.js
+++ local-modules/@bayou/see-all/LogProxyHandler.js
@@ -2,8 +2,6 @@
 // Licensed AS IS and WITHOUT WARRANTY under the Apache License,
 // Version 2.0. Details: <http://www.apache.org/licenses/LICENSE-2.0>
 
-import { inspect } from 'util';
-
 import { TFunction } from '@bayou/typecheck';
 import { MethodCacheProxyHandler } from '@bayou/util-common';
 
