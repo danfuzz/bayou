be112db98580e82c88a1cb7ec6d24326862212ca
Dan Bornstein <danfuzz@slack-corp.com>
2019-04-24 14:51:29 -0700

    Add logging metadata to `ViewSession`.

--- local-modules/@bayou/api-server/ApiLog.js
+++ local-modules/@bayou/api-server/ApiLog.js
@@ -28,7 +28,7 @@ export default class ApiLog extends CommonBase {
     this._log = BaseLogger.check(log);
 
     /** {boolean} Whether the logs should be redacted, generally speaking. */
-    this._shouldRedact = TBoolean.check(shouldRedact);
+    this._shouldRedact = TBoolean.check(shouldRedact) || true;
 
     /**
      * {Map<Message,object>} Map from messages that haven't yet been completely
