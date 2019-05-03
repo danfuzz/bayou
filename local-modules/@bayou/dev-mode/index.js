d1b434448f69826b23ae90d1d50ce598a72cdda4
Dan Bornstein <danfuzz@slack-corp.com>
2019-05-03 13:34:39 -0700

    Convert `export`s in `dev-mode`.

--- local-modules/@bayou/dev-mode/index.js
+++ local-modules/@bayou/dev-mode/index.js
@@ -2,6 +2,6 @@
 // Licensed AS IS and WITHOUT WARRANTY under the Apache License,
 // Version 2.0. Details: <http://www.apache.org/licenses/LICENSE-2.0>
 
-import DevMode from './DevMode';
+import { DevMode } from './DevMode';
 
 export { DevMode };
