d845a48bb33443e094329a57bad78426518bdc64
Dan Bornstein <danfuzz@slack-corp.com>
2019-05-03 15:53:28 -0700

    Convert `export`s in `state-machine`.

--- local-modules/@bayou/state-machine/index.js
+++ local-modules/@bayou/state-machine/index.js
@@ -2,6 +2,6 @@
 // Licensed AS IS and WITHOUT WARRANTY under the Apache License,
 // Version 2.0. Details: <http://www.apache.org/licenses/LICENSE-2.0>
 
-import StateMachine from './StateMachine';
+import { StateMachine } from './StateMachine';
 
 export { StateMachine };
