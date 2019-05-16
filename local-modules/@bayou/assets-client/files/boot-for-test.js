c5e37b7976ad48dafda1e6916ffca104771d0ba2
Dan Bornstein <danfuzz@slack-corp.com>
2019-05-16 13:39:19 -0700

    Adjust `boot-for-test` for lint-happiness.

--- local-modules/@bayou/assets-client/files/boot-for-test.js
+++ local-modules/@bayou/assets-client/files/boot-for-test.js
@@ -5,10 +5,6 @@
 // This file is included by the `/client-test` debugging endpoint and is used
 // to actually kick off the tests.
 
-// Disable Eslint, because this file is delivered as-is and has to be fairly
-// conservative.
-/* eslint-disable */
-
 // We wrap everything in an immediately-executed function so as to avoid
 // polluting the global namespace.
 (function () {
@@ -20,10 +16,10 @@
   // Get the base URL from the window's URL by dropping `/debug` and everything
   // after (e.g. `/debug/test`). This is brittle, in that it bakes in a bit of
   // specific knowledge about the endpoint path.
-  var windowUrl = window.location.href;
-  var baseUrl   = windowUrl.replace(/[/]debug[/].*$/, '');
+  const windowUrl = window.location.href;
+  const baseUrl   = windowUrl.replace(/[/]debug[/].*$/, '');
 
-  var elem = document.createElement('script');
+  const elem = document.createElement('script');
   elem.src = `${baseUrl}/static/js/test.bundle.js`;
   document.head.appendChild(elem);
 }());
