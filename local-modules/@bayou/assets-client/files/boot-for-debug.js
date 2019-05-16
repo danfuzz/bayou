a9c2f922ab61ab1af31b69fac98c469ed0ff9e9c
Dan Bornstein <danfuzz@slack-corp.com>
2019-05-16 13:36:30 -0700

    Adjust `boot-for-debug` for lint-happiness.

--- local-modules/@bayou/assets-client/files/boot-for-debug.js
+++ local-modules/@bayou/assets-client/files/boot-for-debug.js
@@ -7,31 +7,34 @@
 // and most likely because the developer restarted their local server). Beyond
 // that, it _also_ kicks off loading of the regular bootstrap code.
 
-// Disable Eslint, because this file is delivered as-is and has to be fairly
-// conservative.
-/* eslint-disable */
+// Inform ESLint about the special globals used by this file.
+/* global DEBUG_AUTHOR_ID DEBUG_DOCUMENT_ID */
 
 /**
  * This global function is what ultimately gets called to attempt recovery. In
  * this case, we rely on the `/access/*` debugging endpoints to generate new
  * access info. If that's successful, we report it back up through the layers.
+ *
+ * @param {object} info Information about how to connect to the session.
+ * @returns {Promise<object>} Replacement session info, to use when next
+ *   attempting to connect.
  */
-function BAYOU_RECOVER(info) {
-  var documentId = DEBUG_DOCUMENT_ID;
-  var authorId   = DEBUG_AUTHOR_ID;
+window.BAYOU_RECOVER = function BAYOU_RECOVER(info) {
+  const documentId = DEBUG_DOCUMENT_ID;
+  const authorId   = DEBUG_AUTHOR_ID;
 
   // Get the base URL from the server URL by dropping the final `/api`. This is
   // brittle, in that it bakes in knowledge of the API endpoint.
-  var baseUrl = info.serverUrl.replace(/[/]api$/, '');
-  var url     = `${baseUrl}/debug/access/${documentId}/${authorId}`;
+  const baseUrl = info.serverUrl.replace(/[/]api$/, '');
+  const url     = `${baseUrl}/debug/access/${documentId}/${authorId}`;
 
   return new Promise((resolve) => {
-    var req = new XMLHttpRequest();
+    const req = new XMLHttpRequest();
     req.open('GET', url);
     req.send();
     req.addEventListener('abort', reloadPage);
     req.addEventListener('error', reloadPage);
-    req.addEventListener('load', gotKey);
+    req.addEventListener('load', gotInfo);
 
     // If there's any trouble, we just ask the window to reload. This will
     // almost certainly work but is definitely a last-resort hail-mary kind of
@@ -42,21 +45,21 @@ function BAYOU_RECOVER(info) {
 
     // On successful request completion, check to see if we actually got good
     // data. If so, report it back. If not, fall back to `reloadPage()`.
-    function gotKey() {
+    function gotInfo() {
       if (req.status === 200) {
         resolve(req.response);
       } else {
         reloadPage();
       }
     }
-  })
-}
+  });
+};
 
 // Once the rest of the page is loaded, find the DOM node for the editor, and
 // arrange for the main boot script to load and run.
 window.addEventListener('load', () => {
   // This is the node that is IDed specifically in `DebugTools._handle_edit`.
-  var editorNode = document.querySelector('#debugEditor');
+  const editorNode = document.querySelector('#debugEditor');
   if (!editorNode) {
     // Indicates a bug either here or in `DebugTools`. **Note:** This code is
     // run too early to be able to use `@bayou/util-common`'s error facilities.
@@ -71,10 +74,10 @@ window.addEventListener('load', () => {
   // Get the base URL from the window's URL by dropping `/debug` and everything
   // after (e.g. `/debug/edit/...`). This is brittle, in that it bakes in a bit
   // of specific knowledge about the endpoint path.
-  var windowUrl = window.location.href;
-  var baseUrl   = windowUrl.replace(/[/]debug[/].*$/, '');
+  const windowUrl = window.location.href;
+  const baseUrl   = windowUrl.replace(/[/]debug[/].*$/, '');
 
-  var elem = document.createElement('script');
+  const elem = document.createElement('script');
   elem.src = `${baseUrl}/boot-from-info.js`;
   document.head.appendChild(elem);
-})
+});
